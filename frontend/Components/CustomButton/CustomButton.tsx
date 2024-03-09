7d246dfa91f91bfeb9053033b3e2f75c5e454a00
TommyS-NO <tommy.skorge@hotmail.com>
2024-03-09 22:20:05 +0100

    small changes

--- frontend/Components/CustomButton/CustomButton.tsx
+++ frontend/Components/CustomButton/CustomButton.tsx
@@ -5,36 +5,39 @@ import { customButtonStyles } from "../../Styles/CustomButton_Style";
 import { theme } from "../../theme";
 
 interface CustomButtonProps {
-	title?: string;
-	onPress: () => void;
-	iconName?: string;
-	buttonStyle?: ViewStyle;
-	titleStyle?: TextStyle;
+    title?: string;
+    onPress: () => void;
+    iconName?: string;
+    buttonStyle?: ViewStyle;
+    titleStyle?: TextStyle;
+    disabled?: boolean; 
 }
 
 const CustomButton: React.FC<CustomButtonProps> = ({
-	title,
-	onPress,
-	iconName,
-	buttonStyle,
-	titleStyle,
+    title,
+    onPress,
+    iconName,
+    buttonStyle,
+    titleStyle,
+    disabled = false, 
 }) => {
-	return (
-		<TouchableOpacity
-			onPress={onPress}
-			style={[customButtonStyles.button, buttonStyle]}
-		>
-			{iconName && (
-				<Icon
-					name={iconName}
-					size={20}
-					color={theme.colors.icon}
-					style={customButtonStyles.icon}
-				/>
-			)}
-			<Text style={[customButtonStyles.title, titleStyle]}>{title}</Text>
-		</TouchableOpacity>
-	);
+    return (
+        <TouchableOpacity
+            onPress={onPress}
+            style={[customButtonStyles.button, buttonStyle, disabled ? customButtonStyles.disabled : null]}
+            disabled={disabled} // Bruk disabled propen her
+        >
+            {iconName && (
+                <Icon
+                    name={iconName}
+                    size={20}
+                    color={disabled ? theme.colors.disabledIcon : theme.colors.icon} // Endre ikonfarge basert på om knappen er deaktivert
+                    style={customButtonStyles.icon}
+                />
+            )}
+            <Text style={[customButtonStyles.title, titleStyle]}>{title}</Text>
+        </TouchableOpacity>
+    );
 };
 
 export default CustomButton;
