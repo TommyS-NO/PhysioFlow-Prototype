cc79f0b21ed5bb764647a8fdaa713c3904549771
Minathuve <mina.t.andersen@gmail.com>
2024-03-15 20:42:14 +0100

    Updated babel

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,7 +1,24 @@
-module.exports = (api) => {
+// module.exports = (api) => {
+// 	api.cache(true);
+// 	return {
+// 		presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
+// 		plugins: [["module:react-native-dotenv"]],
+// 	};
+// };
+module.exports = function(api) {
 	api.cache(true);
 	return {
-		presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
-		plugins: [["module:react-native-dotenv"]],
+	  presets: ['babel-preset-expo'],
+	  plugins: [
+		['module:react-native-dotenv', {
+		  moduleName: "@env",
+		  path: ".env",
+		  blacklist: null,
+		  whitelist: null,
+		  safe: false,
+		  allowUndefined: true,
+		}]
+	  ],
 	};
-};
+  };
+  
