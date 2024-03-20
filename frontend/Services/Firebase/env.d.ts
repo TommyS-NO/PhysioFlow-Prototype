553ab1f68f0de80fb6b5032c2781f4c2bae02ca8
TommyS-NO <tommy.skorge@hotmail.com>
2024-03-20 23:25:08 +0100

    Added Firebase and Firestore db

---
+++ frontend/Services/Firebase/env.d.ts
@@ -0,0 +1,8 @@
+declare module '@env' {
+		export const REACT_APP_BASE_URL: string;
+		export const API_KEY: string;
+		export const AUTH_DOMAIN: string;
+        export const PROJECT_ID: string;
+        export const MESSAGING_SENDER_ID: string;
+        export const APP_ID: string;
+	}
---
+++ frontend/Services/Firebase/firebaseConfig.ts
@@ -0,0 +1,46 @@
+// firebaseConfig.ts
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword, AuthError } from 'firebase/auth';
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
+import { API_KEY, AUTH_DOMAIN, PROJECT_ID, MESSAGING_SENDER_ID, APP_ID } from '@env';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+export const auth = getAuth(app);
+
+
+export const db = getFirestore(app);
+
+export const registerUser = async (email: string, password: string): Promise<string | null> => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    return userCredential.user.uid; 
+  } catch (error) {
+    const firebaseError = error as AuthError;
+    console.error("Registration Error:", firebaseError.message);
+    
+    return null;
+  }
+};
+
+// Save additional user profile information in Firestore
+export const saveUserProfile = async (userId: string, profile: any): Promise<boolean> => {
+  try {
+    await setDoc(doc(db, "users", userId), profile);
+    return true; 
+  } catch (error) {
+    console.error("Error saving user profile:", error);
+
+    return false;
+  }
+};
