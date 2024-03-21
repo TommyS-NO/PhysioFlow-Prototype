21536221b2ce3b54bcf2ac8a8c67c67ac3e87b68
TommyS-NO <tommy.skorge@hotmail.com>
2024-03-21 20:44:08 +0100

    ..

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -10,7 +10,7 @@ export default {
 		assetBundlePatterns: ["**/*"],
 		ios: {
 			supportsTablet: true,
-			bundleIdentifier: "PhysioGO",
+			bundleIdentifier: "com.pyshiogo.app",
 		},
 		android: {
 			permissions: ["android.permission.DETECT_SCREEN_CAPTURE"],
