0038d3b729e958db693c61261db36842dbc4299d
TommyS-NO <tommy.skorge@hotmail.com>
2024-03-28 09:25:02 +0100

    removed TBA and INfoscreen

--- frontend/Navigation/navigationTypes.ts
+++ frontend/Navigation/navigationTypes.ts
@@ -2,7 +2,6 @@ export type RootStackParamList = {
   Front: undefined;
   Register: { termsAccepted?: boolean };
   TermsScreen: undefined;
-  TBA: undefined;
   FocusScreen: undefined;
   ExerciseSession: undefined;
   UserGuideScreen: undefined;
@@ -13,6 +12,4 @@ export type RootStackParamList = {
   SettingsScreen: undefined;
   ContactScreen: undefined;
   RegisterWorkout: undefined;
-
-  InfoScreen: { title: string; message: string; onContinue?: () => void };
 };
