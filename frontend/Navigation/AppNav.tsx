85af46af320a00c65674f27442af47372aaba272
TommyS-NO <tommy.skorge@hotmail.com>
2024-03-28 09:24:33 +0100

    removed TBA and INfoscreen

--- frontend/Navigation/AppNav.tsx
+++ frontend/Navigation/AppNav.tsx
@@ -4,10 +4,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import FrontScreen from "../Screens/FrontScreen";
 import RegisterScreen from "../Screens/RegisterScreen";
 import TermsScreen from "../Screens/TermsScreen";
-import TBA from "../Screens/TBA";
 import FocusScreen from "../Screens/FocusScreen";
 import UserGuideScreen from "../Screens/UserGuideScreen";
-import InfoScreen from "../Screens/InfoScreen";
+
 import { RootStackParamList } from "./navigationTypes";
 import ExerciseOverviewScreen from "../Screens/ExerciseOverviewScreen";
 import DeleteUserScreen from "../Screens/DeleteUserScreen";
@@ -20,82 +19,76 @@ import RegisterWorkout from "../Screens/RegisterWorkout";
 
 const Stack = createStackNavigator<RootStackParamList>();
 
-const AppNav: React.FC = () => (
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="Front">
-      <Stack.Screen
-        name="Front"
-        component={FrontScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Register"
-        component={RegisterScreen}
-        options={{ title: "Registrer" }}
-      />
-      <Stack.Screen
-        name="TermsScreen"
-        component={TermsScreen}
-        options={{ title: "Vilkår og Betingelser" }}
-      />
-      <Stack.Screen name="TBA" component={TBA} options={{ title: "TBA" }} />
-      <Stack.Screen
-        name="UserGuideScreen"
-        component={UserGuideScreen}
-        options={{ title: "Brukerveiledning" }}
-      />
-      <Stack.Screen
-        name="InfoScreen"
-        component={InfoScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="ExerciseSession"
-        component={ExerciseOverviewScreen}
-        options={{ title: "Mitt treningsprogram" }}
-      />
-      <Stack.Screen
-        name="FocusScreen"
-        component={FocusScreen}
-        options={{ title: "Fokusområde" }}
-      />
-      <Stack.Screen
-        name="DeleteUserScreen"
-        component={DeleteUserScreen}
-        options={{ title: "Slett profil" }}
-      />
-      <Stack.Screen
-        name="AboutScreen"
-        component={AboutScreen}
-        options={{ title: "Om oss" }}
-      />
-      <Stack.Screen
-        name="SearchScreen"
-        component={SearchScreen}
-        options={{ title: "Søk etter øvelser" }}
-      />
-      <Stack.Screen
-        name="ProfileScreen"
-        component={ProfileScreen}
-        options={{ title: "Min profil" }}
-      />
-	        <Stack.Screen
-        name="SettingsScreen"
-        component={SettingsScreen}
-        options={{ title: "Settings" }}
-      />
-      <Stack.Screen
-        name="ContactScreen"
-        component={ContactScreen}
-        options={{ title: "Kontakt oss" }}
-      />
-         <Stack.Screen
-        name="RegisterWorkout"
-        component={RegisterWorkout}
-        options={{ title: "Registrer økt" }}
-      />
-    </Stack.Navigator>
-  </NavigationContainer>
+const AppNav = () => (
+	<NavigationContainer>
+		<Stack.Navigator initialRouteName="Front">
+			<Stack.Screen
+				name="Front"
+				component={FrontScreen}
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen
+				name="Register"
+				component={RegisterScreen}
+				options={{ title: "Registrer" }}
+			/>
+			<Stack.Screen
+				name="TermsScreen"
+				component={TermsScreen}
+				options={{ title: "Vilkår og Betingelser" }}
+			/>
+			<Stack.Screen
+				name="UserGuideScreen"
+				component={UserGuideScreen}
+				options={{ title: "Brukerveiledning" }}
+			/>
+			<Stack.Screen
+				name="ExerciseSession"
+				component={ExerciseOverviewScreen}
+				options={{ title: "Mitt treningsprogram" }}
+			/>
+			<Stack.Screen
+				name="FocusScreen"
+				component={FocusScreen}
+				options={{ title: "Fokusområde" }}
+			/>
+			<Stack.Screen
+				name="DeleteUserScreen"
+				component={DeleteUserScreen}
+				options={{ title: "Slett profil" }}
+			/>
+			<Stack.Screen
+				name="AboutScreen"
+				component={AboutScreen}
+				options={{ title: "Om oss" }}
+			/>
+			<Stack.Screen
+				name="SearchScreen"
+				component={SearchScreen}
+				options={{ title: "Søk etter øvelser" }}
+			/>
+			<Stack.Screen
+				name="ProfileScreen"
+				component={ProfileScreen}
+				options={{ title: "Min profil" }}
+			/>
+			<Stack.Screen
+				name="SettingsScreen"
+				component={SettingsScreen}
+				options={{ title: "Settings" }}
+			/>
+			<Stack.Screen
+				name="ContactScreen"
+				component={ContactScreen}
+				options={{ title: "Kontakt oss" }}
+			/>
+			<Stack.Screen
+				name="RegisterWorkout"
+				component={RegisterWorkout}
+				options={{ title: "Registrer økt" }}
+			/>
+		</Stack.Navigator>
+	</NavigationContainer>
 );
 
 export default AppNav;
