3f2edbe5f57137abd12e5cc690eead6374bdc35d
TommyS-NO <tommy.skorge@hotmail.com>
2024-03-31 21:24:55 +0200

    first layout,

---
+++ frontend/Components/CustomQuestion/CustomQuestion.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { View, Text, Switch, TouchableOpacity } from "react-native";
+
+interface QuestionProps {
+	question: string;
+	onSelection: (question: string, selection: boolean | string) => void;
+	selected: boolean | string | null;
+	options?: string[];
+}
+
+const QuestionComponent: React.FC<QuestionProps> = ({
+	question,
+	onSelection,
+	selected,
+	options,
+}) => {
+	if (options) {
+		// Hvis det er flere valgspørsmål
+		return (
+			<View>
+				<Text>{question}</Text>
+				{options.map((option) => (
+					<TouchableOpacity
+						key={option}
+						onPress={() => onSelection(question, option)}
+					>
+						<Text
+							style={{
+								color: selected === option ? "blue" : "black",
+							}}
+						>
+							{option}
+						</Text>
+					</TouchableOpacity>
+				))}
+			</View>
+		);
+	}
+		// Hvis det er ja/nei-spørsmål
+		return (
+			<View>
+				<Text>{question}</Text>
+				<Switch
+					value={selected === true}
+					onValueChange={(value) => onSelection(question, value)}
+				/>
+			</View>
+		);
+};
+
+export default QuestionComponent;
