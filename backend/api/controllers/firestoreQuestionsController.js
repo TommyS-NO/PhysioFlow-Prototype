48a7ea91f5ae91db54a58b0d8112f57fa33734e8
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-04 12:00:56 +0200

    QUstioncontroller

--- backend/api/controllers/firestoreQuestionsController.js
+++ backend/api/controllers/firestoreQuestionsController.js
@@ -18,15 +18,14 @@ export const getGeneralQuestions = async (req, res) => {
 
 export const getQuestionsByFocusArea = async (req, res) => {
 	const focusArea = req.params.focusArea;
-
 	try {
 		const focusAreaRef = db.collection("focusAreas").doc(focusArea);
 		const doc = await focusAreaRef.get();
-
 		if (!doc.exists) {
+			console.log(`Focus area ${focusArea} not found in Firestore`);
 			return res.status(404).json({ message: "Focus area not found." });
 		}
-
+		console.log(`Fetched data for focus area ${focusArea}:`, doc.data());
 		res.json(doc.data());
 	} catch (error) {
 		console.error(
@@ -60,6 +59,7 @@ export const getIndividualQuestion = async (req, res) => {
 };
 
 export const getFollowUpQuestions = async (req, res) => {
+	// Oppdaterer funksjonen for å inkludere håndtering av `followUpQuestions` samling
 	const { focusArea, questionId } = req.params;
 
 	try {
@@ -76,10 +76,10 @@ export const getFollowUpQuestions = async (req, res) => {
 			return res.status(404).json({ message: "No follow-up questions found." });
 		}
 
-		const followUps = [];
-		for (const doc of snapshot.docs) {
-			followUps.push(doc.data());
-		}
+		const followUps = snapshot.docs.map((doc) => ({
+			id: doc.id,
+			...doc.data(),
+		}));
 
 		res.json(followUps);
 	} catch (error) {
@@ -87,6 +87,50 @@ export const getFollowUpQuestions = async (req, res) => {
 			`Error fetching follow-up questions for question ${questionId}:`,
 			error,
 		);
+		res.status(500).json({ error: error.message });
+	}
+};
+
+export const handleAnswer = async (req, res) => {
+	const { focusArea, questionId, answer } = req.body;
+
+	try {
+		const questionRef = db
+			.collection("focusAreas")
+			.doc(focusArea)
+			.collection("questionsById")
+			.doc(questionId);
+
+		const doc = await questionRef.get();
+
+		if (!doc.exists) {
+			return res.status(404).json({ message: "Question not found." });
+		}
+
+		const questionData = doc.data();
+
+		const followUpId = questionData.followUp[answer];
+
+		if (!followUpId) {
+			return res.status(404).json({
+				message: "Follow-up question not found based on the given answer.",
+			});
+		}
+		const followUpRef = db
+			.collection("focusAreas")
+			.doc(focusArea)
+			.collection("questionsById")
+			.doc(followUpId);
+
+		const followUpDoc = await followUpRef.get();
+
+		if (!followUpDoc.exists) {
+			return res.status(404).json({ message: "Follow-up question not found." });
+		}
+
+		res.json(followUpDoc.data());
+	} catch (error) {
+		console.error(`Error handling answer for question ${questionId}:`, error);
 		res.status(500).json({ error: error.message });
 	}
 };
