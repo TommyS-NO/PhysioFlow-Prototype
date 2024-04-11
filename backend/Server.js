c415f43f30658ab56386928fde172fb52240ec05
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-11 19:19:46 +0200

    new server setup

--- backend/Server.js
+++ backend/Server.js
@@ -2,10 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
-
-// Import your route modules
-import userRoutes from "./api/routes/userRoute.js";
-import questionRoutes from "./api/routes/questionRoutes.js";
+import surveyRoutes from "./routes/surveyRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,20 +10,15 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// Simple test route
-app.get("/test", (req, res) => {
-	res.send("Testruten fungerer!");
-});
-// Define the base path for your routes
-app.use("/api/users", userRoutes);
-app.use("/api/questions", questionRoutes);
+// Use the API routes
+app.use("/api/survey", surveyRoutes);
 
 // 404 Not Found handler
 app.use((req, res) => {
 	res.status(404).send("Siden ble ikke funnet!");
 });
 
-// Global error handling middleware
+// Global error handler
 app.use((err, req, res, next) => {
 	console.error(err.stack);
 	res.status(500).send("Noe gikk galt!");
