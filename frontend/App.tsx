3f416710a004dd489f80653e967a8ed2df4da40f
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-12 21:10:30 +0200

    new provider

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -4,17 +4,20 @@ import { StyleSheet, View } from "react-native";
 import AppNav from "./Navigation/AppNav";
 import { UserProvider } from "./Context/UserContext";
 import { FocusAreaProvider } from "./Context/FocusContext";
+import SurveyProvider from "./Context/SurveyContext";
 
 export default function App() {
 	return (
-		<FocusAreaProvider>
-			<UserProvider>
-				<View style={styles.container}>
-					<AppNav />
-					<StatusBar style="auto" />
-				</View>
-			</UserProvider>
-		</FocusAreaProvider>
+		<SurveyProvider>
+			<FocusAreaProvider>
+				<UserProvider>
+					<View style={styles.container}>
+						<AppNav />
+						<StatusBar style="auto" />
+					</View>
+				</UserProvider>
+			</FocusAreaProvider>
+		</SurveyProvider>
 	);
 }
 
