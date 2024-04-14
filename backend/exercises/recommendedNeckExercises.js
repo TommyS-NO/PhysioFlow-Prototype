28e5dcaad412d9c1a4d248cb8ee1f45d9144ee7f
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-14 17:34:31 +0200

    fixed image import link

--- backend/exercises/recommendedNeckExercises.js
+++ backend/exercises/recommendedNeckExercises.js
@@ -32,167 +32,167 @@ export const recommendedNeckExercises = {
 	"Aerobe øvelser for å forbedre blodsirkulasjonen": {
 		description:
 			"Utfør enhver form for moderat aerob trening, som rask gange eller sykling, i 30 minutter. Dette forbedrer generell sirkulasjon og kan lindre nakkespenninger.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Yoga eller Pilates for fleksibilitet og styrke": {
 		description:
 			"Delta i en yoga eller Pilates klasse som fokuserer på styrking og fleksibilitet i rygg og nakke.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Myke nakkerotasjoner": {
 		description:
 			"Sitt oppreist med god holdning. Roter hodet sakte fra side til side. Fokuser på å holde bevegelsene myke og kontrollerte. Gjenta 5-10 ganger per side.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Pentøyninger foran og bak": {
 		description:
 			"Sitt eller stå rett opp. Lut hodet forsiktig fremover og bakover. Hold hver posisjon i 20 sekunder. Gjør 3-5 repetisjoner.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	Skulderhevinger: {
 		description:
 			"Hev skuldrene mot ørene, hold i noen sekunder, og slipp dem deretter ned igjen. Gjenta dette 10 ganger for å redusere spenninger.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Tøying av Trapezius muskelen": {
 		description:
 			"Sitt eller stå med rett rygg. Lut hodet mot en side mens du trekker den motsatte skulderen ned. Hold i 20-30 sekunder. Bytt side og gjenta.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Nakke- og skuldertøyninger": {
 		description:
 			"Sitt rett opp. Tøy nakken ved å lene hodet til siden og bruk hånden for å øke strekket. Hold i 20-30 sekunder. Gjenta på den andre siden.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	Avspenningsøvelser: {
 		description:
 			"Utfør pusteøvelser eller meditasjon for å hjelpe til med avspenning av musklene i nakke og skuldre.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Styrkeøvelser for nakke og skuldermuskulatur": {
 		description:
 			"Utfør øvelser som skulderpress og nakkebøyninger med lett motstand. Fokuser på god teknikk og langsomme bevegelser.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Øvelser for bedre holdning": {
 		description:
 			"Arbeid aktivt med holdningsøvelser, som å trekke skuldrene tilbake og ned mens du holder ryggen rett. Gjør disse bevisst gjennom dagen.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Myke tøyninger av nakken": {
 		description:
 			"Sitt oppreist med god holdning. Tøy nakken sakte til den ene siden ved å lene hodet mot skulderen og hold strekket i 20-30 sekunder. Gjenta på den andre siden.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Styrkeøvelser for nakke og øvre rygg": {
 		description:
 			"Sitt eller stå med en rett rygg. Bruk et lett motstandsband eller en håndkle for å trekke armene bakover mens du klemmer sammen skulderbladene. Hold i 5 sekunder og slipp sakte.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Selvmassasje av nakke og skuldre": {
 		description:
 			"Bruk fingrene eller en massasjeapparat til å massere forsiktig rundt nakke og skuldre. Fokuser på områder med spesiell spenning og smerte.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Pusteøvelser og stressmestring": {
 		description:
 			"Sitt i en komfortabel posisjon. Ta dype, kontrollerte åndedrag gjennom nesen, hold luften innenfor noen sekunder, og pust ut sakte gjennom munnen. Gjenta i flere minutter for å redusere stress.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Triggerpunkt terapi": {
 		description:
 			"Bruk en tennisball eller lignende objekt til å presse mot triggerpunkter i nakken og skuldrene. Hold presset i 20-30 sekunder før du slipper.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Dype strekkøvelser": {
 		description:
 			"Utfør dyptvirkende strekk, som å bøye seg fremover mens du sitter og nå ned mot føttene. Hold strekket i 20-30 sekunder.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Styrkebyggende øvelser for musklene": {
 		description:
 			"Utfør øvelser som armhevinger og planking for å bygge styrke i nakke og øvre ryggmuskulatur. Sørg for å holde en god form for å unngå belastning.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	Varmebehandling: {
 		description:
 			"Bruk en varmepakning eller varmeteppe på nakken i 15-20 minutter for å lindre muskelspenninger og forbedre blodsirkulasjonen.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Nakkeforlengelse og -fleksjon": {
 		description:
 			"Sitt rett opp og se rett frem. Tilt hodet forsiktig bakover og deretter forsiktig fremover, slik at haken nærmer seg brystet. Hold hver posisjon i 5 sekunder.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Skånsomme rotasjonsøvelser": {
 		description:
 			"Sitt rett opp med hodet i nøytral posisjon. Roter hodet forsiktig til den ene siden og deretter til den andre, uten å overstrekke. Gjenta 5-10 ganger.",
-		image: "/frontend/Assets/12501301.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Ergonomisk tilpasning": {
 		description:
 			"Sørg for at arbeidsstasjonen din er ergonomisk riktig satt opp med dataskjerm i øyehøyde og med støtte for ryggen og føttene.",
-		image: "/frontend/Assets/ergonomisk_tilpasning.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Aqua terapi": {
 		description:
 			"Utfør øvelser i et basseng med varmt vann for å redusere belastningen på nakken og ryggen mens du forbedrer fleksibilitet og styrke.",
-		image: "/frontend/Assets/aqua_terapi.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Øvelser for å øke spinal fleksibilitet": {
 		description:
 			"Utfør rygg- og nakkeøvelser som broer og katt-ku bevegelser for å forbedre spinal fleksibilitet og lindre smerte.",
-		image: "/frontend/Assets/ovelser_for_a_oke_spinal_fleksibilitet.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Styrkeøvelser for kjernemuskulaturen": {
 		description:
 			"Utfør planker, sit-ups, og andre kjernemuskulatur styrkende øvelser for å støtte og stabilisere ryggraden.",
-		image: "/frontend/Assets/styrkeovelser_for_kjernemuskulaturen.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	Balansetrening: {
 		description:
 			"Stå på ett ben, eventuelt med øynene lukket, for å forbedre balanse og styrke i kjerne og nedre rygg.",
-		image: "/frontend/Assets/balansetrening.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	Gåturer: {
 		description:
 			"Regelmessige gåturer kan hjelpe til med å opprettholde fleksibilitet og styrke i ryggraden og nedre ekstremiteter.",
-		image: "/frontend/Assets/gaturer.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Lette nakkefleksjoner": {
 		description:
 			"Gjør milde bøyningsøvelser for nakken ved å tøye ned mot hver skulder og brystet. Hold hver strekk i noen sekunder.",
-		image: "/frontend/Assets/lette_nakkefleksjoner.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Tøying av skuldre og bryst": {
 		description:
 			"Utfør strekkøvelser for skuldrene og brystet for å redusere spenning og forbedre holdning.",
-		image: "/frontend/Assets/toying_av_skuldre_og_bryst.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	Stabilitetsøvelser: {
 		description:
 			"Inkluder øvelser som forbedrer stabiliteten i rygg og kjernemuskulatur, som balansebrett eller stabilitetskuleøvelser.",
-		image: "/frontend/Assets/stabilitetsovelser.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Lave impact aerobe øvelser": {
 		description:
 			"Utfør lav-impact aerobe øvelser som sykling eller rask gange for å forbedre hjerte-kar helse uten stor belastning på ryggraden.",
-		image: "/frontend/Assets/lave_impact_aerobe_ovelser.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Lett aerob trening": {
 		description:
 			"Engasjer i mild aerob trening som rask gange eller svømming for å øke blodsirkulasjonen og lindre muskelspenninger.",
-		image: "/frontend/Assets/lett_aerob_trening.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	"Gentle stretching": {
 		description:
 			"Utfør skånsomme tøyninger for å lindre spenning i nakke og ryggmusklene, slik som nakketøyninger og tøyninger av nedre rygg.",
-		image: "/frontend/Assets/gentle_stretching.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 	Varmeterapi: {
 		description:
 			"Bruk en varmepakning på smertefulle områder for å hjelpe til med å løsne stramme muskler og øke blodsirkulasjonen.",
-		image: "/frontend/Assets/varmeterapi.gif",
+		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 };
 
