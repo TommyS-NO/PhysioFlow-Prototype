06461306f1a6d7d459f1d43f8d9245a3a3c3a3c3
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-14 17:24:16 +0200

    new routes import

--- backend/Server.js
+++ backend/Server.js
@@ -1,24 +1,30 @@
 import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import surveyRoutes from "./routes/surveyRoute.js";
+import exerciseRoutes from "./routes/exerciseRoute.js";
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/static", express.static(path.join(__dirname, "public")));
 
-// Use the API routes
 app.use("/api/survey", surveyRoutes);
+app.use("/api/exercises", exerciseRoutes);
 
-// 404 Not Found handler
 app.use((req, res) => {
 	res.status(404).send("Siden ble ikke funnet!");
 });
 
-// Global error handler
 app.use((err, req, res, next) => {
 	console.error(err.stack);
 	res.status(500).send("Noe gikk galt!");
