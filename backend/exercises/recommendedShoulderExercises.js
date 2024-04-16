21916c9dbd2e6ed301ece90f3415787f1b113a77
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-16 21:27:29 +0200

    new file

---
+++ backend/exercises/recommendedShoulderExercises.js
@@ -0,0 +1,68 @@
+export const recommendedShoulderExercises = {
+	ShoulderPendulum: {
+		description:
+			"Stå og bøy overkroppen fremover mens du lar den ene armen henge ned. Sving armen forsiktig i små sirkler. Gjenta øvelsen i noen minutter.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	WallClimbExercises: {
+		description:
+			"Stå foran en vegg og plasser hendene på veggen. 'Klatre' sakte med fingrene oppover veggen til du føler en strekk i skulderen. Hold posisjonen, og returner sakte til startposisjon.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	IsometricShoulderExercises: {
+		description:
+			"Stå eller sitte med ryggen rett. Plasser håndflaten mot en vegg og press forsiktig uten å bevege armen. Hold presset i 10 sekunder, slapp av og gjenta.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	GentleShoulderStretches: {
+		description:
+			"Stå oppreist og rull skuldrene sakte i en sirkulær bevegelse. Gjør 10 repetisjoner fremover og deretter 10 bakover.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	ShoulderBladeSqueezes: {
+		description:
+			"Sitt eller stå med ryggen rett. Trekk skulderbladene sammen og nedover som om du prøver å holde en blyant mellom dem. Hold i noen sekunder, slipp opp og gjenta 10-15 ganger.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	StretchingBandExercises: {
+		description:
+			"Bruk et elastisk treningsbånd. Hold båndet i begge hender, strekk armene ut foran deg og trekk båndet utover mens du beveger hendene til siden. Hold strekken, og returner sakte til midten.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	LightWeightShoulderLifts: {
+		description:
+			"Hold en lett håndvekt i hver hånd. Løft vektene sidelengs opp til skulderhøyde og senk dem sakte ned igjen. Gjenta 10-12 ganger.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	AerobicExercisesForCirculation: {
+		description:
+			"Utfør lett aerob trening som gange eller svømming for å forbedre blodsirkulasjonen og redusere inflammasjon.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	ShoulderMobilityExercises: {
+		description:
+			"Utfør sirkulære bevegelser med skuldrene, både med klokken og mot klokken, for å forbedre fleksibiliteten og redusere stivhet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	HeatOrColdTherapy: {
+		description:
+			"Bruk en varmepute eller en ispose på skulderen i 15-20 minutter for å hjelpe med å lindre smerte og redusere inflammasjon.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	StrengthExercisesUnderGuidance: {
+		description:
+			"Utfør skulderstyrkende øvelser under veiledning av en fysioterapeut for å sikre riktig teknikk og forhindre ytterligere skader.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	ElasticBandTraining: {
+		description:
+			"Bruk et elastisk bånd for å utføre bicep- og skulderøvelser, som hjelper med å styrke og forbedre fleksibiliteten.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	PosturalControlExercises: {
+		description:
+			"Utfør øvelser som forbedrer kroppsholdningen og reduserer belastningen på skulderen, spesielt ved skapulær dyskinesi.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+};
