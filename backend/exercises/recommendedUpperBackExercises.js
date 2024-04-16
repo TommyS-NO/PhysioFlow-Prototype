7f57c03b31b94794765554e00b77982af8a324a8
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-16 23:47:38 +0200

    new structure

--- backend/exercises/recommendedUpperBackExercises.js
+++ backend/exercises/recommendedUpperBackExercises.js
@@ -1,74 +1,157 @@
 export const recommendedUpperBackExercises = {
-	ShoulderBladeRetractions: {
+	Skulderbladsretraksjoner: {
 		description:
-			"Stå eller sitt med rett rygg. Trekk skulderbladene sammen bak på ryggen og hold i noen sekunder før du slapper av. Gjenta øvelsen 10-15 ganger.",
+			"Stå eller sitte med rett rygg. Trekk skulderbladene sammen mot hverandre og nedover, som om du prøver å holde en blyant mellom dem. Hold i noen sekunder og slipp sakte.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
-	NeckAndShoulderStretches: {
+
+	"Nakke- og skuldertøyninger": {
 		description:
-			"Utfør milde tøyninger for nakke og skuldre ved å lene hodet forsiktig til hver side og bruke hånden for å forsterke strekket. Hold i 20-30 sekunder på hver side.",
+			"Stå eller sitte med god holdning. Trekk hodet forsiktig mot hver skulder, hold i 20-30 sekunder hver side, og gjør det samme ved å bøye nakken forover og bakover.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
-	PostureImprovementExercises: {
+
+	"Øvelser for å forbedre holdning": {
 		description:
-			"Arbeid på din holdning ved å sikre at ryggen er rett, skuldrene trukket tilbake, og hodet i nøytral stilling. Praktiser dette foran et speil for å korrigere dårlige vaner.",
+			"Praktiser holdningsøvelser som 'veggengler'. Stå med ryggen mot en vegg og armene i en 'W'-posisjon. Skyv langsomt armene oppover til en 'Y'-posisjon mens du holder kontakt med veggen.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
-	UpperBackStrengthExercises: {
+
+	"Styrkeøvelser for skuldermuskulatur": {
 		description:
-			"Utfør øvelser som armhevinger mot vegg eller med knebøy, som styrker musklene i øvre rygg og skuldre.",
+			"Utfør øvelser som skulderpress og lateral heving med håndvekter for å bygge styrke i skuldermuskulaturen.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
-	ScapularStabilizationExercises: {
+
+	"Øvelser for skulderbladstabilisering": {
 		description:
-			"Utfør øvelser som fokuserer på å stabilisere skulderbladene, for eksempel ved å presse hendene mot en vegg mens du står i en plankelignende posisjon.",
+			"Utfør 'I', 'T', 'Y' øvelser ved å ligge på magen med utstrakte armer. Løft armene sakte i hver av de tre posisjonene for å styrke musklene rundt skulderbladene.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
-	ChestAndShoulderStretches: {
+
+	"Tøyninger for bryst og skuldre": {
 		description:
-			"Stå i en døråpning og plasser underarmene på dørkarmen. Ta et steg fremover til du kjenner et strekk i brystet og skuldrene. Hold i 20-30 sekunder.",
+			"Stå i en døråpning og plasser armene på dørkarmen. Ta et skritt fremover til du føler en strekk i brystet og skuldrene. Hold i 20-30 sekunder.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
-	GentleBackStretching: {
+
+	"Styrkeøvelser for øvre rygg": {
 		description:
-			"Ligg på ryggen med bøyde knær og forsiktig rull bekkenet mot deg for å føle et lett strekk i nedre del av ryggen. Hold i noen sekunder og slipp.",
+			"Utfør ro-øvelser med en motstandsbånd eller ro-maskin for å styrke musklene i øvre rygg og forbedre holdning.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
-	CoreStrengtheningForBackSupport: {
+
+	"Balansetrening for skuldermuskulaturen": {
 		description:
-			"Utfør øvelser som planken eller lette sit-ups for å styrke kjernemuskulaturen, noe som er viktig for å støtte og avlaste øvre rygg.",
+			"Utfør balanseøvelser som å stå på ett ben mens du forsiktig beveger armene for å utfordre og styrke skuldermuskulaturen.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
-	WaterExercisesForGentleTraining: {
+
+	"Myke tøyninger": {
 		description:
-			"Utfør øvelser i vann, som vannjogging eller svømming, som er skånsomt for ryggen og samtidig gir god motstandstrening.",
+			"Utfør myke tøyninger for ryggen ved å bøye seg forsiktig til hver side og holde hver strekk i 20-30 sekunder.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
-	PositionalExercisesToReduceLoad: {
+
+	"Selvmassasje og triggerpunkt terapi": {
 		description:
-			"Praktiser stillingsøvelser som lindrer trykket på ryggraden, for eksempel ved å ligge på siden med en pute mellom knærne.",
+			"Bruk en skumrulle eller tennisball for å massere og lindre stramme muskler og triggerpunkter i øvre rygg.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
-	BreathingExercises: {
+
+	Varmebehandling: {
 		description:
-			"Utfør dype pusteøvelser for å fremme avslapning og redusere spenninger i øvre rygg og skulderområdet.",
+			"Anvend en varmepakning på ømme eller stive områder i øvre rygg for å forbedre blodsirkulasjonen og lindre muskelspenninger.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
-	GentleRotationalExercisesForUpperBody: {
+
+	"Lette styrkeøvelser for øvre rygg": {
 		description:
-			"Sitt eller stå, og roter forsiktig overkroppen fra side til side. Dette kan hjelpe med å øke fleksibiliteten og redusere stivhet.",
+			"Utfør lette roøvelser med en motstandsbånd eller på et kabelmaskin, fokuser på å trekke med øvre ryggmuskulaturen.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
-	RelaxationExercisesToReduceStress: {
+
+	"Gentle stretching for ryggen": {
 		description:
-			"Utfør avspenningsøvelser, som guidet meditasjon eller yoga, for å redusere stress som kan bidra til muskelspenninger.",
+			"Utfør ryggstrekk som å ligge på ryggen med bøyde knær og sakte rulle knærne til hver side. Dette bidrar til å redusere spenning og forbedre fleksibilitet i ryggen.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
-	HeatTherapyForPainRelief: {
+
+	"Styrkeøvelser for kjernemuskulaturen": {
 		description:
-			"Bruk en varmepakning på øvre rygg for å lindre smerte og slappe av stramme muskler.",
+			"Utfør kjernestyrkende øvelser som planken eller sit-ups for å støtte og stabilisere ryggraden.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Vannøvelser for skånsom trening": {
+		description:
+			"Engasjer i vannøvelser som vannaerobic eller svømming for å redusere belastning på ryggen mens du øker styrke og fleksibilitet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Stillingsøvelser for å redusere belastning": {
+		description:
+			"Praktiser stillingsøvelser som å sørge for riktig ryggstøtte når du sitter og å stå regelmessig opp fra sittestillingen for å forhindre stivhet og øke mobilitet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	Pusteøvelser: {
+		description:
+			"Utfør dype pusteøvelser for å redusere stress og fremme avslapning, noe som kan hjelpe med å redusere muskelspenninger rundt ryggraden og ribbene.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Tøyninger for bryst og rygg": {
+		description:
+			"Utfør tøyninger for bryst og rygg ved å bruke en døråpning eller ved å gjøre 'veggengler' for å redusere spenninger og forbedre holdning.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Gentle rotasjonsøvelser for overkroppen": {
+		description:
+			"Stå eller sitte og utfør forsiktige rotasjonsbevegelser med overkroppen for å forbedre fleksibiliteten og redusere spenninger i rygg og ribben.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Avspenningsøvelser for å redusere stress": {
+		description:
+			"Engasjer i avspenningsøvelser som meditasjon eller yoga for å hjelpe med å redusere den generelle muskelspenningen og fremme avslapning.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Øvelser for å øke brystkassens mobilitet": {
+		description:
+			"Utfør øvelser som inkluderer armløft og forsiktig rotasjon for å øke mobiliteten i brystkassen og lette pusten.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Dype pusteøvelser for å fremme avslapning": {
+		description:
+			"Fokuser på å ta dype, langsomme pust for å øke oksygenopptaket og fremme avslapning av musklene i øvre rygg og brystområde.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Gentle tøyninger for å redusere muskelspenninger": {
+		description:
+			"Utfør milde tøyninger, spesielt for øvre rygg og skuldre, for å redusere muskelspenninger og fremme økt bevegelighet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Styrkeøvelser for øvre rygg og skuldre": {
+		description:
+			"Utfør styrkeøvelser som fokuserer på øvre rygg og skuldre for å bygge muskelstyrke og støtte for ryggraden.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Gentle stretching for å forbedre fleksibilitet": {
+		description:
+			"Utfør skånsomme strekkøvelser for å øke fleksibiliteten i øvre rygg, skuldre og bryst, noe som kan hjelpe med å lindre smerte og forbedre holdning.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Varmeterapi for å lindre smerte og øke blodsirkulasjonen": {
+		description:
+			"Bruk varmepakker på øvre rygg for å lindre smerte og stivhet, samtidig som blodsirkulasjonen til de berørte områdene økes.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 };
-
-export default recommendedUpperBackExercises;
