417cdb19f65ee87e368ba1e94a06620507e1eeef
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-16 23:28:19 +0200

    fixed structure

--- backend/exercises/recommendedHipExercises.js
+++ backend/exercises/recommendedHipExercises.js
@@ -1,69 +1,195 @@
-// export const recommendedHipExercises = {
-// 	Gåturer: {
-// 		description:
-// 			"Regelmessige gåturer ved moderat tempo kan hjelpe med å opprettholde mobilitet og styrke i hoften, samtidig som det er lav belastning.",
-// 		image: "http://localhost:3000/static/assets/12501301.gif",
-// 	},
+export const recommendedHipExercises = {
+	Gåturer: {
+		description:
+			"Regelmessige gåturer ved moderat tempo kan hjelpe med å opprettholde mobilitet og styrke i hoften, samtidig som det er lav belastning.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
 
-// 	"Sykling på stasjonær sykkel": {
-// 		description:
-// 			"Bruke en stasjonær sykkel for å fremme bevegelighet og kondisjon uten å legge for mye belastning på hoftene.",
-// 		image: "http://localhost:3000/static/assets/12501301.gif",
-// 	},
+	"Sykling på stasjonær sykkel": {
+		description:
+			"Bruke en stasjonær sykkel for å fremme bevegelighet og kondisjon uten å legge for mye belastning på hoftene.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
 
-// 	Vannøvelser: {
-// 		description:
-// 			"Utfør øvelser i et basseng for å utnytte vannets naturlige motstand og oppdrift, noe som minimerer stress på hoftene mens du bygger styrke og fleksibilitet.",
-// 		image: "http://localhost:3000/static/assets/12501301.gif",
-// 	},
+	Vannøvelser: {
+		description:
+			"Utfør øvelser i et basseng for å utnytte vannets naturlige motstand og oppdrift, noe som minimerer stress på hoftene mens du bygger styrke og fleksibilitet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
 
-// 	"Lette strekkøvelser for hoften": {
-// 		description:
-// 			"Utfør forsiktige strekkøvelser for å øke fleksibiliteten og redusere stivheten i hoftene, som f.eks. lysbøyninger eller roterende bevegelser.",
-// 		image: "http://localhost:3000/static/assets/12501301.gif",
-// 	},
+	"Lette strekkøvelser for hoften": {
+		description:
+			"Utfør forsiktige strekkøvelser for å øke fleksibiliteten og redusere stivheten i hoftene, som f.eks. lysbøyninger eller roterende bevegelser.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
 
-// 	"Islegging for å redusere betennelse": {
-// 		description:
-// 			"Anvend en ispakke på det smertefulle området for å redusere betennelse og lindre smerte, spesielt etter øvelser.",
-// 		image: "http://localhost:3000/static/assets/12501301.gif",
-// 	},
+	"Islegging for å redusere betennelse": {
+		description:
+			"Anvend en ispakke på det smertefulle området for å redusere betennelse og lindre smerte, spesielt etter øvelser.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
 
-// 	"Gentle stretching": {
-// 		description:
-// 			"Utfør milde tøyninger for å bedre mobiliteten og redusere smerten i hoften. Fokus på forsiktig uttøyning av hofteflexorer og hamstrings.",
-// 		image: "http://localhost:3000/static/assets/12501301.gif",
-// 	},
+	"Gentle stretching": {
+		description:
+			"Utfør milde tøyninger for å bedre mobiliteten og redusere smerten i hoften. Fokus på forsiktig uttøyning av hofteflexorer og hamstrings.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
 
-// 	"Forsterkende øvelser for hoftemuskulaturen": {
-// 		description:
-// 			"Inkluder øvelser som hip bridges eller skånsomme benløft for å styrke musklene rundt hoften og støtte opp om leddet.",
-// 		image: "http://localhost:3000/static/assets/12501301.gif",
-// 	},
+	"Forsterkende øvelser for hoftemuskulaturen": {
+		description:
+			"Inkluder øvelser som hip bridges eller skånsomme benløft for å styrke musklene rundt hoften og støtte opp om leddet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
 
-// 	"Øvelser for å forbedre mobilitet": {
-// 		description:
-// 			"Utfør øvelser som gradvis øker bevegelsesområdet i hoften, f.eks. stående hofteleddøvelser eller bruk av motstandsbånd.",
-// 		image: "http://localhost:3000/static/assets/12501301.gif",
-// 	},
+	"Øvelser for å forbedre mobilitet": {
+		description:
+			"Utfør øvelser som gradvis øker bevegelsesområdet i hoften, f.eks. stående hofteleddøvelser eller bruk av motstandsbånd.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
 
-// 	"Øvelser for å styrke omkringliggende muskler": {
-// 		description:
-// 			"Fokus på å styrke muskler som støtter hoftens stabilitet, inkludert gluteus- og core-muskulaturen.",
-// 		image: "http://localhost:3000/static/assets/12501301.gif",
-// 	},
+	"Øvelser for å styrke omkringliggende muskler": {
+		description:
+			"Fokus på å styrke muskler som støtter hoftens stabilitet, inkludert gluteus- og core-muskulaturen.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
 
-// 	"Myke rotasjonsøvelser for hoften": {
-// 		description:
-// 			"Utfør myke, kontrollerte rotasjoner med hoften for å forbedre bevegelighet og redusere stivhet uten å overbelaste leddet.",
-// 		image: "http://localhost:3000/static/assets/12501301.gif",
-// 	},
+	"Myke rotasjonsøvelser for hoften": {
+		description:
+			"Utfør myke, kontrollerte rotasjoner med hoften for å forbedre bevegelighet og redusere stivhet uten å overbelaste leddet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
 
-// 	"Core-styrkende øvelser": {
-// 		description:
-// 			"Engasjer kjernemuskulaturen med øvelser som planker eller abdominal crunches for å støtte og stabilisere hoftene.",
-// 		image: "http://localhost:3000/static/assets/12501301.gif",
-// 	},
-// };
+	"Core-styrkende øvelser": {
+		description:
+			"Engasjer kjernemuskulaturen med øvelser som planker eller abdominal crunches for å støtte og stabilisere hoftene.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
 
-// export default recommendedHipExercises;
+	Balansetrening: {
+		description:
+			"Utfør balanseøvelser som stående på ett ben eller bruk av balansebrett for å forbedre kroppens generelle stabilitet og redusere risikoen for fall.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Strekking av iliopsoas muskelen": {
+		description:
+			"Utfør spesifikke tøyninger for å målrette iliopsoas-muskelen, som ligger dypt i buken og påvirker hoften, for å redusere stramhet og forbedre bevegelighet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Forsterkende øvelser for lårene": {
+		description:
+			"Inkluder øvelser som knebøy og leg extensions for å bygge styrke i lårmusklene, noe som hjelper med å støtte og stabilisere hoften.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Milde mobilitetsøvelser for hoften": {
+		description:
+			"Utfør lette øvelser for å forbedre hoftens bevegelsesområde uten å forårsake smerte, som forsiktige sirkulære bevegelser.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Core-stabiliseringsøvelser": {
+		description:
+			"Arbeid på core-stabilitet gjennom øvelser som lav-impact planker og bird dogs for å styrke musklene som støtter hoften.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Strekking av piriformis og setemuskulatur": {
+		description:
+			"Utfør tøyninger som retter seg mot piriformis-muskelen, som kan påvirke isjiasnerven, for å redusere risikoen for irritasjon og smerte.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Styrkeøvelser for sete- og hoftemuskulatur": {
+		description:
+			"Utfør øvelser som hip thrusts og squats for å styrke sete- og hoftemuskulaturen, og gi bedre støtte til hoftene.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Mobiliseringsøvelser for nedre rygg og hofter": {
+		description:
+			"Inkluder øvelser som korsryggrotasjoner og pelvis tilts for å forbedre fleksibilitet og redusere spenninger som kan påvirke hoften.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	Avspenningsøvelser: {
+		description:
+			"Praktiser avspenningsøvelser som dyp pusting eller lett yoga for å hjelpe med å redusere muskelspenninger og forbedre generell kroppsholdning.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Vannøvelser for å redusere vektbærende stress": {
+		description:
+			"Utfør øvelser i et basseng for å minimere belastning på hoftene mens du forbedrer både styrke og fleksibilitet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Gentle range-of-motion øvelser for å opprettholde mobilitet": {
+		description:
+			"Utfør milde bevegelsesøvelser som forsiktig beveger hoften gjennom dens naturlige bevegelsesrekkevidde for å opprettholde fleksibilitet og redusere stivhet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Isometriske øvelser for å styrke musklene rundt hoften uten bevegelse": {
+		description:
+			"Utfør isometriske øvelser ved å stramme hoftemusklene uten å bevege leddene, noe som kan styrke musklene uten å legge press på hoften.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Bruk av krykker eller stokk for å minimere belastning": {
+		description:
+			"Anvend krykker eller en stokk for å hjelpe med vektfordeling og for å minimere belastningen på den berørte hoften under gange.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Øvelser for øvre kropp og ikke-berørte lemmer for å opprettholde generell styrke og kondisjon":
+		{
+			description:
+				"Fokus på å opprettholde styrke i øvre kropp og de ikke-berørte lemmer gjennom øvelser som armhevinger eller bruk av håndvekter.",
+			image: "http://localhost:3000/static/assets/12501301.gif",
+		},
+
+	"Gradvis økning av vektbærende aktiviteter etter kirurgi, som anbefalt av en fysioterapeut":
+		{
+			description:
+				"Etter kirurgisk behandling, gradvis øke aktiviteter som belaster hoften, alltid i henhold til retningslinjer gitt av en fysioterapeut for sikker rehabilitering.",
+			image: "http://localhost:3000/static/assets/12501301.gif",
+		},
+
+	"Gentle stretching og mobilitetsøvelser for hoften etter kirurgens godkjenning":
+		{
+			description:
+				"Etter at kirurgen har gitt sitt samtykke, begynn med milde tøyninger og mobilitetsøvelser for hoften for å fremme heling og forbedre bevegelighet.",
+			image: "http://localhost:3000/static/assets/12501301.gif",
+		},
+
+	"Første faser involverer immobilisering og gradvis økning av ikke-vikt bærende aktiviteter":
+		{
+			description:
+				"Umiddelbart etter skade, fokus på immobilisering og sakte økning av aktiviteter som ikke belaster hoften for å tillate heling.",
+			image: "http://localhost:3000/static/assets/12501301.gif",
+		},
+
+	"Range-of-motion øvelser etter at immobilisering er fjernet, under tilsyn av helsepersonell":
+		{
+			description:
+				"Når immobiliseringen er fjernet, start med forsiktige bevegelsesøvelser for å gradvis forbedre hoftens fleksibilitet, alltid under tilsyn av kvalifisert helsepersonell.",
+			image: "http://localhost:3000/static/assets/12501301.gif",
+		},
+
+	"Styrkebyggende øvelser for hoftens stabiliserende muskler": {
+		description:
+			"Arbeid på å bygge styrke i musklene rundt hoften for å stabilisere leddet og forbedre dens funksjon etter at immobiliseringen er fjernet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Balansetrening for å styrke kroppens evne til å håndtere ujevne overflater og forhindre fremtidige dislokasjoner":
+		{
+			description:
+				"Utfør balansetrening for å bedre håndtering av ujevne overflater og minske risikoen for fremtidige dislokasjoner ved å styrke kroppens overordnede stabilitet.",
+			image: "http://localhost:3000/static/assets/12501301.gif",
+		},
+};
+
+export default recommendedHipExercises;
