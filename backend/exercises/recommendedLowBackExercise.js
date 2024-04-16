116c61b6b9dd990f6dde9dcc490add1940fef8be
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-16 21:27:08 +0200

    new setup

---
+++ backend/exercises/recommendedLowBackExercise.js
@@ -0,0 +1,74 @@
+export const recommendedLowBackExercises = {
+	TøyningAvRyggmusklene: {
+		description:
+			"Gjør milde tøyninger av ryggmusklene for å lindre spenning og fremme fleksibilitet i nedre del av ryggen.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	IsleggingForÅRedusereInflammasjon: {
+		description:
+			"Bruk en ispakke på det betente området i nedre del av ryggen for å redusere hevelse og smerte. Husk å ha et beskyttende lag mellom isen og huden.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	StyrkeøvelserForKjerne: {
+		description:
+			"Utfør øvelser som styrker kjernemuskulaturen for å gi bedre støtte til ryggsøylen og forebygge fremtidige ryggsmerter.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	LettAerobTreningSomGange: {
+		description:
+			"Gåturer kan hjelpe med å forbedre sirkulasjonen og styrke ryggen uten å legge for mye belastning på ryggsøylen.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	TøyningAvHamstring: {
+		description:
+			"Milde tøyninger av hamstring kan bidra til å redusere spenning i ryggen ved å øke fleksibiliteten i baksiden av lårene.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	LavImpactAerobTrening: {
+		description:
+			"Lav-impact aerob trening som svømming eller sykling kan forbedre din generelle fitness og ryggens helse uten å overstresse ryggen.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	MildYogaForFleksibilitetOgStyrke: {
+		description:
+			"Delta i yogaøvelser som er skånsomme for ryggen for å forbedre både fleksibilitet og styrke i kroppen.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	TøyingAvRyggOgBen: {
+		description:
+			"Tøyninger som involverer både ryggen og bena kan hjelpe til med å forbedre den generelle mobiliteten og redusere stivhet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	PilatesForKjerneforsterkningOgFleksibilitet: {
+		description:
+			"Pilates kan hjelpe med å styrke kjernemuskulaturen og forbedre fleksibiliteten, noe som kan redusere risikoen for ryggskader.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	BalansetreningForÅForbedreStabilitet: {
+		description:
+			"Øvelser som forbedrer balansen kan hjelpe med å stabilisere ryggen og redusere risikoen for skader som følge av fall.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	FysioterapiForÅLæreRiktigeBevegelsesmønstre: {
+		description:
+			"Et program for fysioterapi kan gi deg øvelser som er tilpasset din tilstand for å sikre riktig bevegelse og styrking av ryggen.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	VannøvelserForÅRedusereBelastning: {
+		description:
+			"Øvelser utført i vann kan redusere belastningen på ryggen mens du trener, og er spesielt gunstig for de med ryggproblemer.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	PosisjonsøvelserForÅRedusereNerveklem: {
+		description:
+			"Visse posisjonsøvelser kan hjelpe til med å redusere trykket på isjiasnerven og lette symptomer på isjias.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	VarmebehandlingForÅRedusereMuskelspenninger: {
+		description:
+			"Bruk varme på anspente områder av ryggen for å hjelpe med å slappe av i musklene og forbedre blodsirkulasjonen.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+};
+
+export default recommendedLowBackExercises;
