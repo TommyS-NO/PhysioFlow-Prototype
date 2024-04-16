e15abe54d736af26394507764f05717f86f9ee96
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-16 21:26:44 +0200

    ..

--- backend/exercises/recommendedKneeExercises.js
+++ backend/exercises/recommendedKneeExercises.js
@@ -1,73 +1,102 @@
-export const recommendedKneeExercises = {
-	"Gentle stretching of the quadriceps": {
-		description:
-			"Ligg på ryggen uten pute under hodet. Løft hodet sakte opp, se rett frem, og hold i 3-5 sekunder før du senker det rolig. Gjenta 10 ganger.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
+// export const recommendedKneeExercises = {
+// 	"Gentle stretching of the quadriceps": {
+// 		description:
+// 			"Ligg på ryggen uten pute under hodet. Løft hodet sakte opp, se rett frem, og hold i 3-5 sekunder før du senker det rolig. Gjenta 10 ganger.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
 
-	"Styrkeøvelser for lårmuskulaturen": {
-		description:
-			"Stå oppreist med føttene skulderbredde fra hverandre. Utfør knebøyninger med eller uten vekter, sørg for at knærne ikke går forbi tærne. Gjenta 8-12 ganger i 2-3 sett.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
+// 	"Styrkeøvelser for lårmuskulaturen": {
+// 		description:
+// 			"Stå oppreist med føttene skulderbredde fra hverandre. Utfør knebøyninger med eller uten vekter, sørg for at knærne ikke går forbi tærne. Gjenta 8-12 ganger i 2-3 sett.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
 
-	"Islegging etter aktivitet": {
-		description:
-			"Påfør en ispakke på det smertefulle området i 15-20 minutter etter aktivitet for å redusere betennelse og smerte. Bruk et håndkle mellom ispakken og huden for å unngå frostskader.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
+// 	"Islegging etter aktivitet": {
+// 		description:
+// 			"Påfør en ispakke på det smertefulle området i 15-20 minutter etter aktivitet for å redusere betennelse og smerte. Bruk et håndkle mellom ispakken og huden for å unngå frostskader.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
 
-	"Range-of-motion øvelser": {
-		description:
-			"Ligg på ryggen og løft det skadede benet sakte opp. Hold benet rett og løft det til det er i en vinkel på 45 grader med gulvet. Hold posisjonen i noen sekunder og senk det sakte tilbake. Gjenta 5-10 ganger.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
+// 	"Range-of-motion øvelser": {
+// 		description:
+// 			"Ligg på ryggen og løft det skadede benet sakte opp. Hold benet rett og løft det til det er i en vinkel på 45 grader med gulvet. Hold posisjonen i noen sekunder og senk det sakte tilbake. Gjenta 5-10 ganger.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
 
-	"Styrkebyggende øvelser for ben": {
-		description:
-			"Utfør leg press i en leg press-maskin. Start med lav vekt for å unngå overbelastning. Fokusér på å presse jevnt med begge bena. Gjenta 10-15 ganger i 2-3 sett.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
+// 	"Styrkebyggende øvelser for ben": {
+// 		description:
+// 			"Utfør leg press i en leg press-maskin. Start med lav vekt for å unngå overbelastning. Fokusér på å presse jevnt med begge bena. Gjenta 10-15 ganger i 2-3 sett.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
 
-	Balansetrening: {
-		description:
-			"Stå på ett ben, og hold balansen så lenge du kan. Bytt ben etter 30 sekunder. For å øke vanskelighetsgraden, prøv øvelsen med lukkede øyne eller på en balansepute.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
+// 	Balansetrening: {
+// 		description:
+// 			"Stå på ett ben, og hold balansen så lenge du kan. Bytt ben etter 30 sekunder. For å øke vanskelighetsgraden, prøv øvelsen med lukkede øyne eller på en balansepute.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
 
-	"Gradvis økning av vektbærende aktiviteter": {
-		description:
-			"Begynn med å gå korte distanser på flat overflate. Øk gradvis lengden på gåturene og introducer lett jogging etter hvert som smerten tillater det.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
+// 	"Gradvis økning av vektbærende aktiviteter": {
+// 		description:
+// 			"Begynn med å gå korte distanser på flat overflate. Øk gradvis lengden på gåturene og introducer lett jogging etter hvert som smerten tillater det.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
 
-	Mobilitetsøvelser: {
-		description:
-			"Ligg på ryggen med begge knærne bøyd. Roter benet utover og innover fra hoften, og hold kneet og ankelen i ro. Dette vil hjelpe til med å bevare hoftens bevegelighet.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
+// 	Mobilitetsøvelser: {
+// 		description:
+// 			"Ligg på ryggen med begge knærne bøyd. Roter benet utover og innover fra hoften, og hold kneet og ankelen i ro. Dette vil hjelpe til med å bevare hoftens bevegelighet.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
 
-	"Styrking av hamstrings og quadriceps": {
-		description:
-			"Utfør leg curls for å styrke hamstrings og leg extensions for å styrke quadriceps. Juster maskinene slik at de passer din kroppslengde og start med lav vekt for å unngå belastning.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
+// 	"Styrking av hamstrings og quadriceps": {
+// 		description:
+// 			"Utfør leg curls for å styrke hamstrings og leg extensions for å styrke quadriceps. Juster maskinene slik at de passer din kroppslengde og start med lav vekt for å unngå belastning.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
 
-	"Varme- eller kuldebehandling": {
-		description:
-			"Alternativ mellom varmepakning og ispakning på det berørte området. Påfør varme eller kulde i 15-20 minutter, og sørg for å ha et håndkle mellom pakningen og huden.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
+// 	"Varme- eller kuldebehandling": {
+// 		description:
+// 			"Alternativ mellom varmepakning og ispakning på det berørte området. Påfør varme eller kulde i 15-20 minutter, og sørg for å ha et håndkle mellom pakningen og huden.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
 
-	"Core-styrkeøvelser": {
-		description:
-			"Utfør planken ved å holde kroppen rett og stiv fra topp til tå. Hold posisjonen så lenge du kan, med fokus på å stramme magemusklene. Gjenta 3-5 ganger.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
+// 	"Core-styrkeøvelser": {
+// 		description:
+// 			"Utfør planken ved å holde kroppen rett og stiv fra topp til tå. Hold posisjonen så lenge du kan, med fokus på å stramme magemusklene. Gjenta 3-5 ganger.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
 
-	"Tøying av iliotibialbåndet og hamstrings": {
-		description:
-			"Stå og kryss ett ben over det andre. Len deg forsiktig mot den motsatte siden til du føler en strekk langs siden av låret. Hold i 15-30 sekunder og bytt side.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
-};
+// 	"Tøying av iliotibialbåndet og hamstrings": {
+// 		description:
+// 			"Stå og kryss ett ben over det andre. Len deg forsiktig mot den motsatte siden til du føler en strekk langs siden av låret. Hold i 15-30 sekunder og bytt side.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
+// 	"Isometriske øvelser": {
+// 		description:
+// 			"Utfør en knebøy mot en vegg med et håndkle rullet bak knærne. Press knærne inn i håndkleet og hold spenningen i 5-10 sekunder. Slipp og gjenta 10 ganger.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
+
+// 	"Aerobic øvelser som svømming eller sykling": {
+// 		description:
+// 			"Utfør lav-impact aerobic trening som sykling på stasjonær sykkel eller svømming for å opprettholde kondisjonen uten stor belastning på kneet.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
+
+// 	"Styrkeøvelser for quadriceps": {
+// 		description:
+// 			"Utfør leg extensions ved å sitte på en stol og løfte ett ben om gangen. Hold benet utstrakt i noen sekunder før du senker det rolig. Gjenta 10-15 ganger for hvert ben.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
+
+// 	"Stretching of the lower extremity muscles": {
+// 		description:
+// 			"Utfør en rekke strekkøvelser for nedre ekstremiteter, inkludert hamstring, calf, og ankler for å forbedre fleksibilitet og redusere stramhet.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
+
+// 	"Gradvis gjenopptakelse av hoppeaktiviteter": {
+// 		description:
+// 			"Begynn med små, kontrollerte hopp på begge ben. Øk gradvis høyden og intensiteten på hoppene etter hvert som styrken og smerten tillater det.",
+// 		image: "http://localhost:3000/static/assets/12501301.gif",
+// 	},
+// };
