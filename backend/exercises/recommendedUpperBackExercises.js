ac0bf246b43e36afcf04d0bdf9e9d961661e603a
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-16 21:27:40 +0200

    new files

---
+++ backend/exercises/recommendedUpperBackExercises.js
@@ -0,0 +1,74 @@
+export const recommendedUpperBackExercises = {
+	ShoulderBladeRetractions: {
+		description:
+			"Stå eller sitt med rett rygg. Trekk skulderbladene sammen bak på ryggen og hold i noen sekunder før du slapper av. Gjenta øvelsen 10-15 ganger.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	NeckAndShoulderStretches: {
+		description:
+			"Utfør milde tøyninger for nakke og skuldre ved å lene hodet forsiktig til hver side og bruke hånden for å forsterke strekket. Hold i 20-30 sekunder på hver side.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	PostureImprovementExercises: {
+		description:
+			"Arbeid på din holdning ved å sikre at ryggen er rett, skuldrene trukket tilbake, og hodet i nøytral stilling. Praktiser dette foran et speil for å korrigere dårlige vaner.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	UpperBackStrengthExercises: {
+		description:
+			"Utfør øvelser som armhevinger mot vegg eller med knebøy, som styrker musklene i øvre rygg og skuldre.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	ScapularStabilizationExercises: {
+		description:
+			"Utfør øvelser som fokuserer på å stabilisere skulderbladene, for eksempel ved å presse hendene mot en vegg mens du står i en plankelignende posisjon.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	ChestAndShoulderStretches: {
+		description:
+			"Stå i en døråpning og plasser underarmene på dørkarmen. Ta et steg fremover til du kjenner et strekk i brystet og skuldrene. Hold i 20-30 sekunder.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	GentleBackStretching: {
+		description:
+			"Ligg på ryggen med bøyde knær og forsiktig rull bekkenet mot deg for å føle et lett strekk i nedre del av ryggen. Hold i noen sekunder og slipp.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	CoreStrengtheningForBackSupport: {
+		description:
+			"Utfør øvelser som planken eller lette sit-ups for å styrke kjernemuskulaturen, noe som er viktig for å støtte og avlaste øvre rygg.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	WaterExercisesForGentleTraining: {
+		description:
+			"Utfør øvelser i vann, som vannjogging eller svømming, som er skånsomt for ryggen og samtidig gir god motstandstrening.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	PositionalExercisesToReduceLoad: {
+		description:
+			"Praktiser stillingsøvelser som lindrer trykket på ryggraden, for eksempel ved å ligge på siden med en pute mellom knærne.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	BreathingExercises: {
+		description:
+			"Utfør dype pusteøvelser for å fremme avslapning og redusere spenninger i øvre rygg og skulderområdet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	GentleRotationalExercisesForUpperBody: {
+		description:
+			"Sitt eller stå, og roter forsiktig overkroppen fra side til side. Dette kan hjelpe med å øke fleksibiliteten og redusere stivhet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	RelaxationExercisesToReduceStress: {
+		description:
+			"Utfør avspenningsøvelser, som guidet meditasjon eller yoga, for å redusere stress som kan bidra til muskelspenninger.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	HeatTherapyForPainRelief: {
+		description:
+			"Bruk en varmepakning på øvre rygg for å lindre smerte og slappe av stramme muskler.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+};
+
+export default recommendedUpperBackExercises;
---
+++ backend/exercises/recommendedWristExercises.js
@@ -0,0 +1,64 @@
+export const recommendedWristExercises = {
+	WristStretches: {
+		description:
+			"Utfør tøyninger for å øke fleksibiliteten og redusere spenningen i håndleddet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	NerveGlidingExercisesForWrist: {
+		description:
+			"Nerveglidningsøvelser hjelper til med å forbedre nervens bevegelighet og redusere nummenhet eller prikking.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	StrengthExercisesForHandAndForearm: {
+		description:
+			"Styrkeøvelser for hånden og underarmen forbedrer grepstyrken og støtten rundt håndleddet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	WristBalanceTraining: {
+		description:
+			"Balansetrening for håndleddet styrker de små musklene rundt håndleddet og forbedrer stabiliteten.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	IceApplication: {
+		description:
+			"Bruk en ispakke for å redusere inflammasjon og smerte. Ikke påfør isen direkte på huden, bruk et håndkle mellom.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	GentleStrengtheningExercises: {
+		description:
+			"Start med milde styrkeøvelser for håndleddet etter symptombedring for å unngå overbelastning.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	WristSupportUse: {
+		description:
+			"Bruk støttebandasje eller skinne som anbefalt av helsepersonell for å stabilisere håndleddet under helingsprosessen.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	MobilityExercisesForFingersAndElbow: {
+		description:
+			"Utfør mobilitetsøvelser for fingrene og albuen for å opprettholde bevegelighet og sirkulasjon etter en håndleddsskade.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	WaterExercises: {
+		description:
+			"Vannøvelser gir skånsom trening som hjelper med rehabilitering av håndleddet uten å legge for mye press på det.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	WristAndThumbStrengthening: {
+		description:
+			"Spesifikke styrkeøvelser for håndledd og tommel, fokusert på å støtte og avlaste de berørte senene.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	ErgonomicAdjustments: {
+		description:
+			"Gjør ergonomiske tilpasninger i arbeids- eller aktivitetsmiljøet for å redusere stress og belastning på håndleddet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+	HeatTherapyForWrist: {
+		description:
+			"Bruk varmeterapi for å lindre smerte og redusere stivhet i håndleddet, spesielt effektivt ved artrose.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+};
+
+export default recommendedWristExercises;
