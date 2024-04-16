7accd35b17efe7f535b5849464bec65b1d9b5daa
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-16 19:25:58 +0200

    updatet controller

--- backend/controllers/exerciseController.js
+++ backend/controllers/exerciseController.js
@@ -1,9 +1,25 @@
 import { recommendedNeckExercises } from "../exercises/recommendedNeckExercises.js";
+import { recommendedAnkleExercises } from "../exercises/recommendedAnkelExercises.js";
+import { recommendedElbowExercises } from "../exercises/recommendedelbowExercises.js";
+import { recommendedKneeExercises } from "../exercises/recommendedKneeExercises.js";
+import { recommendedLowBackExercises } from "../exercises/recommendedLowBackExercise.js";
+import { recommendedUpperBackExercises } from "../exercises/recommendedUpperBackExercises.js";
+import { recommendedWristExercises } from "../exercises/recommendedWristExercises.js";
+
+const allExercises = {
+	...recommendedNeckExercises,
+	...recommendedAnkleExercises,
+	...recommendedElbowExercises,
+	...recommendedKneeExercises,
+	...recommendedLowBackExercises,
+	...recommendedUpperBackExercises,
+	...recommendedWristExercises,
+};
 
 export const getExerciseDetails = (req, res) => {
 	try {
 		const { exercises } = req.body;
-		const details = exercises.map((name) => recommendedNeckExercises[name]);
+		const details = exercises.map((name) => allExercises[name]);
 		res.json(details);
 	} catch (error) {
 		console.error("Error fetching exercise details:", error);
