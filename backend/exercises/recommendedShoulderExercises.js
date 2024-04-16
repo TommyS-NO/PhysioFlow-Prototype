7a62f8360de26062a44b340319ab65aa54713209
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-16 23:29:15 +0200

    fixed structure

--- backend/exercises/recommendedShoulderExercises.js
+++ backend/exercises/recommendedShoulderExercises.js
@@ -1,68 +1,139 @@
 export const recommendedShoulderExercises = {
-	ShoulderPendulum: {
+	Skulderpendeløvelser: {
 		description:
-			"Stå og bøy overkroppen fremover mens du lar den ene armen henge ned. Sving armen forsiktig i små sirkler. Gjenta øvelsen i noen minutter.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
-	WallClimbExercises: {
-		description:
-			"Stå foran en vegg og plasser hendene på veggen. 'Klatre' sakte med fingrene oppover veggen til du føler en strekk i skulderen. Hold posisjonen, og returner sakte til startposisjon.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
-	IsometricShoulderExercises: {
-		description:
-			"Stå eller sitte med ryggen rett. Plasser håndflaten mot en vegg og press forsiktig uten å bevege armen. Hold presset i 10 sekunder, slapp av og gjenta.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
-	GentleShoulderStretches: {
-		description:
-			"Stå oppreist og rull skuldrene sakte i en sirkulær bevegelse. Gjør 10 repetisjoner fremover og deretter 10 bakover.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
-	ShoulderBladeSqueezes: {
-		description:
-			"Sitt eller stå med ryggen rett. Trekk skulderbladene sammen og nedover som om du prøver å holde en blyant mellom dem. Hold i noen sekunder, slipp opp og gjenta 10-15 ganger.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
-	StretchingBandExercises: {
-		description:
-			"Bruk et elastisk treningsbånd. Hold båndet i begge hender, strekk armene ut foran deg og trekk båndet utover mens du beveger hendene til siden. Hold strekken, og returner sakte til midten.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
-	LightWeightShoulderLifts: {
-		description:
-			"Hold en lett håndvekt i hver hånd. Løft vektene sidelengs opp til skulderhøyde og senk dem sakte ned igjen. Gjenta 10-12 ganger.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
-	AerobicExercisesForCirculation: {
-		description:
-			"Utfør lett aerob trening som gange eller svømming for å forbedre blodsirkulasjonen og redusere inflammasjon.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
-	ShoulderMobilityExercises: {
-		description:
-			"Utfør sirkulære bevegelser med skuldrene, både med klokken og mot klokken, for å forbedre fleksibiliteten og redusere stivhet.",
-		image: "http://localhost:3000/static/assets/12501301.gif",
-	},
-	HeatOrColdTherapy: {
-		description:
-			"Bruk en varmepute eller en ispose på skulderen i 15-20 minutter for å hjelpe med å lindre smerte og redusere inflammasjon.",
+			"Lene deg fremover og la den berørte armen henge ned. Sving armen forsiktig frem og tilbake, og deretter i små sirkler. Dette kan hjelpe med å redusere stivhet uten å overbelaste skulderen.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 
-	StrengthExercisesUnderGuidance: {
+	Veggklatreøvelser: {
 		description:
-			"Utfør skulderstyrkende øvelser under veiledning av en fysioterapeut for å sikre riktig teknikk og forhindre ytterligere skader.",
+			"Stå vendt mot en vegg med fingertuppene på veggen. 'Klatre' sakte oppover veggen med fingrene til du føler en mild strekk, hold, og gå sakte tilbake ned.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
-	ElasticBandTraining: {
+
+	"Isometriske øvelser for rotatormansjetten": {
 		description:
-			"Bruk et elastisk bånd for å utføre bicep- og skulderøvelser, som hjelper med å styrke og forbedre fleksibiliteten.",
+			"Stå ved siden av en vegg med albuen i en rett vinkel. Press håndflaten inn i veggen og hold i 5-10 sekunder. Gjenta flere ganger for å styrke rotatormansjetten uten å bevege skulderen.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
-	PosturalControlExercises: {
+
+	"Gentle stretching of the shoulder": {
 		description:
-			"Utfør øvelser som forbedrer kroppsholdningen og reduserer belastningen på skulderen, spesielt ved skapulær dyskinesi.",
+			"Ta tak i en stolpe eller dørkarm med en hånd og sakte roter kroppen bort fra hånden for å strekke skulderen. Hold i 20-30 sekunder, og gjenta flere ganger.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Strekkøvelser for skulder": {
+		description:
+			"Ta tak i en stolpe, dørkarm eller håndkle bak ryggen. Sakte løft armene for å strekke skulderen. Hold posisjonen i 20-30 sekunder og slipp forsiktig.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Gentle mobilisering": {
+		description:
+			"Utfør forsiktige mobiliseringsøvelser, som å rotere skulderen forsiktig i hele sitt bevegelsesomfang. Dette kan gjøres ved å rotere armen i store, langsomme sirkler.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Armhevinger fra vegg": {
+		description:
+			"Stå med ansiktet mot en vegg og plasser hendene på veggen, omtrent på skulderbreddes avstand. Bøy albuen og senk kroppen mot veggen, og skyv deretter tilbake til startposisjon.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Styrkeøvelser under veiledning": {
+		description:
+			"Arbeid med en fysioterapeut eller trener for å utføre skulderstyrkeøvelser som er skreddersydd for å unngå ytterligere belastning, ved hjelp av lett vekt eller motstandsbånd.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	Skulderrotasjoner: {
+		description:
+			"Stå eller sitte med armene ned langs sidene. Roter skuldrene i sirkler, først fremover, deretter bakover. Dette hjelper med å opprettholde fleksibilitet og bevegelsesomfang.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	Skulderbladssammentrekninger: {
+		description:
+			"Stå rett opp med armene på sidene. Trekk skulderbladene sammen bak deg, hold i noen sekunder, og slipp sakte.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Tøying av skulderbånd": {
+		description:
+			"Stå eller sitte, og bruk den ene hånden til å trekke den motsatte armen forsiktig over kroppen mot brystet til en mild strekk oppnås i skulderen.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Skulderløft med lett vekt": {
+		description:
+			"Stå rett med en lett vekt i hver hånd. Løft armene sakte til siden til de er på skulderhøyde, og senk dem deretter kontrollert.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Aerobe øvelser som svømming": {
+		description:
+			"Svømming gir utmerket lav-impact aerob trening som hjelper med å bygge styrke og fleksibilitet i skulderen uten å legge for mye stress på leddene.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	Skuldermobilitetsøvelser: {
+		description:
+			"Inkluder øvelser som forsiktig øker bevegeligheten i skulderen, som å svinge armen forsiktig i en sirkelbevegelse og øke radien gradvis.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Lette styrkeøvelser for skulderen": {
+		description:
+			"Utfør lette styrkeøvelser som skulderpress med meget lett vekt for å forbedre styrken uten å overbelaste leddet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Bruk av varme eller kulde for smertelindring": {
+		description:
+			"Veksle mellom å anvende varmepakninger og ispakker på skulderen for å behandle smerte og stivhet. Varme før aktivitet for å øke fleksibilitet, og is etter aktivitet for å redusere inflammasjon.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Stabilitetsøvelser for skulderen": {
+		description:
+			"Utfør øvelser som bidrar til å stabilisere skulderen, som å holde et motstandsbånd fast mens du sakte beveger armen bort fra kroppen.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Isometriske hold for skulder": {
+		description:
+			"Stå ved siden av en vegg, trykk albuen mot veggen, og hold posisjonen i 10-15 sekunder. Dette styrker skulderen uten bevegelse, noe som er ideelt ved smerte.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Gentle rotasjonsøvelser": {
+		description:
+			"Stå med albuen bøyd i en rett vinkel, og bruk den andre hånden til å hjelpe til med å rotere armen forsiktig innover og utover innenfor smertefrie grenser.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Tøyning av skulder og nakke": {
+		description:
+			"Utfør forsiktige tøyninger for skulder og nakke ved å vippe hodet mot hver skulder og bruke hånden til å øke strekken forsiktig.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Øvelser for å korrigere skulderbladets posisjon": {
+		description:
+			"Stå med ryggen mot en vegg og prøv å presse skulderbladene mot veggen uten å bøye armene. Dette kan hjelpe med å korrigere stillingen og forbedre skulderfunksjonen.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Mobiliseringsteknikker for å forbedre bevegelighet": {
+		description:
+			"Få veiledning fra en fysioterapeut til å utføre spesifikke mobiliseringsteknikker som kan frigjøre stramme skuldermusklatur og forbedre bevegelighet.",
+		image: "http://localhost:3000/static/assets/12501301.gif",
+	},
+
+	"Øvelser for å forbedre postural kontroll": {
+		description:
+			"Engasjer i øvelser som fokuserer på å forbedre postural kontroll, som å stå rett med skuldrene trukket tilbake og ned, og praktisere denne holdningen regelmessig.",
 		image: "http://localhost:3000/static/assets/12501301.gif",
 	},
 };
