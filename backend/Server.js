97a06048230737c1f5bef8b5471d0c8523fb188e
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-18 13:40:38 +0200

    ..

--- backend/Server.js
+++ backend/Server.js
@@ -14,6 +14,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+3
 app.use(cors());
 app.use(express.json());
 app.use("/static", express.static(path.join(__dirname, "public")));
