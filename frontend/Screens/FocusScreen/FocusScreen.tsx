dada5729156eed41d361f86e30693659491972d3
Minathuve <mina.t.andersen@gmail.com>
2024-04-18 20:41:28 +0200

    Added link to FocusScreen_Style

--- frontend/Screens/FocusScreen/FocusScreen.tsx
+++ frontend/Screens/FocusScreen/FocusScreen.tsx
@@ -13,6 +13,7 @@ import CustomButton from "../../Components/CustomButton/CustomButton";
 import CustomSlider from "../../Components/CustomSlider/CustomSlider";
 import { SurveyContext } from "../../Context/SurveyContext";
 import { surveyService } from "../../Services/SurveyService";
+import { styles } from "../FocusScreen/FocusScreen_Style";
 
 type Answer = string | number;
 type AnswerMap = Record<string, Answer>;
@@ -235,46 +236,50 @@ const FocusScreen = () => {
 	);
 };
 
-const styles = StyleSheet.create({
-	container: { flex: 1, backgroundColor: "#f2f2f2" },
-	content: { flexGrow: 1, paddingTop: 20 },
-	questionContainer: {
-		borderBottomWidth: 1,
-		borderBottomColor: "#ececec",
-		padding: 10,
-	},
-	questionText: { fontSize: 14, color: "#333" },
-	optionButton: {
-		padding: 10,
-		borderRadius: 5,
-		margin: 2,
-		backgroundColor: "#dddddd",
-	},
-	selectedOption: { backgroundColor: "green" },
-	optionText: { fontSize: 12, color: "black", textAlign: "center" },
-	modalView: {
-		marginHorizontal: 20,
-		marginTop: "10%",
-		marginBottom: "10%",
-		paddingHorizontal: 15,
-		paddingTop: 15,
-		paddingBottom: 20,
-		borderRadius: 20,
-		backgroundColor: "white",
-		shadowColor: "#000",
-		shadowOffset: { width: 0, height: 2 },
-		shadowOpacity: 0.25,
-		shadowRadius: 3.84,
-		elevation: 5,
-		width: "100%",
-		maxHeight: "80%",
-	},
-	diagnosisTitle: { fontWeight: "bold", fontSize: 18, marginTop: 20 },
-	diagnosisText: { fontSize: 16, marginTop: 10, marginBottom: 20 },
-	exerciseDetailContainer: { marginBottom: 20 },
-	exerciseTitle: { fontSize: 18, fontWeight: "bold" },
-	exerciseDescription: { fontSize: 14, marginVertical: 10 },
-	exerciseImage: { width: 250, height: 150, resizeMode: "contain" },
-});
+
+//Har flyttet denne over i egen .ts fil..
+
+
+// const styles = StyleSheet.create({
+// 	container: { flex: 1, backgroundColor: "#f2f2f2" },
+// 	content: { flexGrow: 1, paddingTop: 20 },
+// 	questionContainer: {
+// 		borderBottomWidth: 1,
+// 		borderBottomColor: "#ececec",
+// 		padding: 10,
+// 	},
+// 	questionText: { fontSize: 14, color: "#333" },
+// 	optionButton: {
+// 		padding: 10,
+// 		borderRadius: 5,
+// 		margin: 2,
+// 		backgroundColor: "#dddddd",
+// 	},
+// 	selectedOption: { backgroundColor: "green" },
+// 	optionText: { fontSize: 12, color: "black", textAlign: "center" },
+// 	modalView: {
+// 		marginHorizontal: 20,
+// 		marginTop: "10%",
+// 		marginBottom: "10%",
+// 		paddingHorizontal: 15,
+// 		paddingTop: 15,
+// 		paddingBottom: 20,
+// 		borderRadius: 20,
+// 		backgroundColor: "white",
+// 		shadowColor: "#000",
+// 		shadowOffset: { width: 0, height: 2 },
+// 		shadowOpacity: 0.25,
+// 		shadowRadius: 3.84,
+// 		elevation: 5,
+// 		width: "100%",
+// 		maxHeight: "80%",
+// 	},
+// 	diagnosisTitle: { fontWeight: "bold", fontSize: 18, marginTop: 20 },
+// 	diagnosisText: { fontSize: 16, marginTop: 10, marginBottom: 20 },
+// 	exerciseDetailContainer: { marginBottom: 20 },
+// 	exerciseTitle: { fontSize: 18, fontWeight: "bold" },
+// 	exerciseDescription: { fontSize: 14, marginVertical: 10 },
+// 	exerciseImage: { width: 250, height: 150, resizeMode: "contain" },
+// });
 
 export default FocusScreen;
