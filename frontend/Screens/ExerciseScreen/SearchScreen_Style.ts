76969d4c486a8f2874502656b37586fa82b1f268
Minathuve <mina.t.andersen@gmail.com>
2024-04-20 20:09:08 +0200

    Added SearchScreen_Styles. Commented out

---
+++ frontend/Screens/ExerciseScreen/SearchScreen_Style.ts
@@ -0,0 +1,62 @@
+
+
+
+// Husk å koble disse sammen!!
+
+// import { StyleSheet } from 'react-native';
+
+// const styles = StyleSheet.create({
+// 	container: {
+// 		paddingTop: 20,
+// 		paddingHorizontal: 10,
+// 		backgroundColor: "#fff",
+// 		flex: 1,
+// 	},
+// 	contentContainer: {
+// 		flexGrow: 1,
+// 		justifyContent: "center",
+// 	},
+// 	searchBar: {
+// 		flexDirection: "row",
+// 		borderWidth: 1,
+// 		borderColor: "#ccc",
+// 		borderRadius: 10,
+// 		padding: 10,
+// 		marginBottom: 10,
+// 	},
+// 	searchInput: {
+// 		flex: 1,
+// 		fontSize: 16,
+// 	},
+// 	sessionItem: {
+// 		flex: 1,
+// 		flexDirection: "column",
+// 		margin: 5,
+// 		alignItems: "center",
+// 		maxWidth: "50%",
+// 		padding: 10,
+// 		borderBottomWidth: 1,
+// 		borderBottomColor: "#ccc",
+// 	},
+// 	sessionTitle: {
+// 		flex: 1,
+// 		fontSize: 18,
+// 	},
+// 	sessionImage: {
+// 		width: 100,
+// 		height: 100,
+// 		borderRadius: 50,
+// 	},
+// 	modalImage: {
+// 		width: "100%",
+// 		height: 200,
+// 		resizeMode: "contain",
+// 		marginBottom: 20,
+// 	},
+// 	modalText: {
+// 		textAlign: "center",
+// 		marginBottom: 20,
+// 	},
+// });
+
+
