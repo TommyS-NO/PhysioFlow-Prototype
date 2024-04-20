d3af15122caa2024a572485ac02bb97cbbcb4115
Minathuve <mina.t.andersen@gmail.com>
2024-04-20 22:45:48 +0200

    Added function to send e-mail

--- frontend/Screens/ContactScreen.tsx
+++ frontend/Screens/ContactScreen.tsx
@@ -6,8 +6,11 @@ import {
   KeyboardAvoidingView,
   ScrollView,
   Platform,
+  TouchableOpacity,
+  Alert
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { openComposer } from 'react-native-email-link';
 import { styles } from "../Styles/ContactScreen_Style";
 import CustomButton from "../Components/CustomButton/CustomButton";
 
@@ -17,61 +20,68 @@ const ContactScreen = () => {
   const [phone, setPhone] = useState('');
   const [message, setMessage] = useState('');
 
-
-  const handleSendPress = () => {
-  //  NB NB Her må vi legge til logikk for mottak av informasjon som bruker skriver inn!
+  const handleSendEmail = () => {
+    if (!name.trim() || !email.trim() || !message.trim()) {
+      Alert.alert("Feil", "Alle felt må fylles ut.");
+      return;
+    }
+    openComposer({
+      to: 'terapeut@eksempel.no',
+      subject: 'Henvendelse fra app',
+      body: `Navn: ${name}\nEpost: ${email}\nTelefon: ${phone}\nMelding: ${message}`
+    });
   };
 
   return (
     <KeyboardAvoidingView
-    style={{ flex: 1 }}
-    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-    keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}>
-    <ScrollView style={styles.container}>
-      <Text style={styles.headerText}>Kontakt oss</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Navn"
-        value={name}
-        onChangeText={setName}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Epost-adresse"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Mobilnummer"
-        value={phone}
-        onChangeText={setPhone}
-        keyboardType="phone-pad"
-      />
-      <TextInput
-        style={styles.textArea}
-        placeholder="Din melding"
-        value={message}
-        onChangeText={setMessage}
-        multiline
-        numberOfLines={10}
-      />
-         <CustomButton
-        title="Send"
-        onPress={handleSendPress}
-        iconName="send"
-        buttonStyle={styles.sendButton}
-        titleStyle={styles.sendButtonText}
-      />
-      <View style={styles.iconContainer}>
-        <Icon name="linkedin-square" size={30} style={styles.icon} />
-        <Icon name="phone-square" size={30} style={styles.icon} />
-        <Icon name="facebook-square" size={30} style={styles.icon} />
-        <Icon name="instagram" size={30} style={styles.icon} />
-        <Icon name="twitter-square" size={30} style={styles.icon} />
-      </View>
-    </ScrollView>
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}>
+      <ScrollView style={styles.container}>
+        <Text style={styles.headerText}>Kontakt oss</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Navn"
+          value={name}
+          onChangeText={setName}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Epost-adresse"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Mobilnummer"
+          value={phone}
+          onChangeText={setPhone}
+          keyboardType="phone-pad"
+        />
+        <TextInput
+          style={styles.textArea}
+          placeholder="Din melding"
+          value={message}
+          onChangeText={setMessage}
+          multiline
+          numberOfLines={10}
+        />
+        <CustomButton
+          title="Send melding til din behandler"
+          onPress={handleSendEmail}
+          iconName="send"
+          buttonStyle={styles.sendButton}
+          titleStyle={styles.sendButtonText}
+        />
+        <View style={styles.iconContainer}>
+          <Icon name="linkedin-square" size={30} style={styles.icon} />
+          <Icon name="phone-square" size={30} style={styles.icon} />
+          <Icon name="facebook-square" size={30} style={styles.icon} />
+          <Icon name="instagram" size={30} style={styles.icon} />
+          <Icon name="twitter-square" size={30} style={styles.icon} />
+        </View>
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 };
