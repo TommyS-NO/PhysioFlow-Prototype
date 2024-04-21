6bc7d4f9a0e126612c353092e1339dbc3af401a5
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-21 06:58:15 +0200

    updated

--- backend/routes/exerciseRoute.js
+++ backend/routes/exerciseRoute.js
@@ -4,6 +4,13 @@ import { getExerciseDetails } from "../controllers/exerciseController.js";
 const router = express.Router();
 
 // Henter detaljer for en liste med øvelser
-router.post("/details", getExerciseDetails);
+router.post("/details", (req, res, next) => {
+	try {
+		const details = getExerciseDetails(req.body);
+		res.json(details);
+	} catch (error) {
+		next(error);
+	}
+});
 
 export default router;
