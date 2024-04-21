8642fc26bcf69e8a0e6c4c34fa07d1745066b75b
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-21 20:36:58 +0200

    updated

--- frontend/Components/CustomSlider/CustomSlider.tsx
+++ frontend/Components/CustomSlider/CustomSlider.tsx
@@ -6,6 +6,8 @@ interface CustomSliderProps {
 	value: number;
 	onValueChange: (value: number) => void;
 	title: string;
+	maximumValue: number;
+	minimumValue: number;
 }
 
 const CustomSlider: React.FC<CustomSliderProps> = ({
@@ -20,70 +22,72 @@ const CustomSlider: React.FC<CustomSliderProps> = ({
 		return `rgb(${red},${green},0)`;
 	};
 
-  const renderScale = () => {
-    return (
-      <View style={styles.scaleContainer}>
-        {Array.from({ length: 10 }, (_, i) => i + 1).map((number) => (
-          <Text key={number} style={styles.scaleText}>{number}</Text>
-        ))}
-      </View>
-    );
-  };
+	const renderScale = () => {
+		return (
+			<View style={styles.scaleContainer}>
+				{Array.from({ length: 10 }, (_, i) => i + 1).map((number) => (
+					<Text key={number} style={styles.scaleText}>
+						{number}
+					</Text>
+				))}
+			</View>
+		);
+	};
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{title}</Text>
-      <View style={styles.sliderBackground}>
-        <Slider
-          style={styles.slider}
-          value={value}
-          onValueChange={onValueChange}
-          minimumValue={1}
-          maximumValue={10}
-          step={1}
-          minimumTrackTintColor={getColor(value)}
-          maximumTrackTintColor="#000000"
-          thumbTintColor={getColor(value)}
-        />
-      </View>
-      {renderScale()}
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>{title}</Text>
+			<View style={styles.sliderBackground}>
+				<Slider
+					style={styles.slider}
+					value={value}
+					onValueChange={onValueChange}
+					minimumValue={1}
+					maximumValue={10}
+					step={1}
+					minimumTrackTintColor={getColor(value)}
+					maximumTrackTintColor="#000000"
+					thumbTintColor={getColor(value)}
+				/>
+			</View>
+			{renderScale()}
+		</View>
+	);
 };
 
 const fullWidth = Dimensions.get("window").width - 40;
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: "stretch",
-    justifyContent: "center",
-    marginVertical: 10,
-  },
-  sliderBackground: {
-    height: 30,
-    width: fullWidth,
-    borderRadius: 15,
-    backgroundColor: "#ddd",
-    justifyContent: "center",
-  },
-  slider: {
-    width: fullWidth,
-    height: 40,
-    position: "absolute",
-  },
-  title: {
-    fontSize: 16,
-    color: "black",
-    marginBottom: 4,
-  },
-  scaleContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 8,
-  },
-  scaleText: {
-    fontSize: 12,
-  },
+	container: {
+		alignItems: "stretch",
+		justifyContent: "center",
+		marginVertical: 10,
+	},
+	sliderBackground: {
+		height: 30,
+		width: fullWidth,
+		borderRadius: 15,
+		backgroundColor: "#ddd",
+		justifyContent: "center",
+	},
+	slider: {
+		width: fullWidth,
+		height: 40,
+		position: "absolute",
+	},
+	title: {
+		fontSize: 16,
+		color: "black",
+		marginBottom: 4,
+	},
+	scaleContainer: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		marginTop: 8,
+	},
+	scaleText: {
+		fontSize: 12,
+	},
 });
 
 export default CustomSlider;
