e32b25449ba4d415e588d07316c3b2fa34c4b970
TommyS-NO <tommy.skorge@hotmail.com>
2024-04-24 06:22:40 +0200

    new survey

---
+++ backend/surveys/followUpSurvey.js
@@ -0,0 +1,42 @@
+export const followupSurvey = {
+	id: "followup",
+	title: "Oppfølgingsundersøkelse etter øvelse",
+	questions: [
+		{
+			id: "followupQ1",
+			question:
+				"Vurder smerteintensiteten under øvelsen på en skala fra 1 til 10:",
+			type: "slider",
+			minValue: 1,
+			maxValue: 10,
+		},
+		{
+			id: "followupQ2",
+			question: "Hvor mange repetisjoner klarte du å gjennomføre av øvelsen?",
+			type: "numericInput",
+		},
+		{
+			id: "followupQ3",
+			question:
+				"Vurder vanskelighetsgraden av øvelsen på en skala fra 1 til 10:",
+			type: "slider",
+			minValue: 1,
+			maxValue: 10,
+		},
+		{
+			id: "followupQ4",
+			question: "Hvor lang tid brukte du på å gjennomføre øvelsen?",
+			type: "numericInput",
+		},
+		{
+			id: "followupQ5",
+			question:
+				"Vurder din generelle evne til å utføre øvelsen sammenlignet med din normale funksjon (pasientspesifikk funksjonsskala):",
+			type: "slider",
+			minValue: 1,
+			maxValue: 10,
+		},
+	],
+};
+
+export default followupSurvey;
