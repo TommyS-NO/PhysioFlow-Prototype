b8d0fab8b4828ce96032895b0aecf96c7961b01d
Minathuve <mina.t.andersen@gmail.com>
2024-05-01 12:01:18 +0200

    Updated styling modal in FC

--- frontend/Screens/FocusScreen/FocusScreen_Style.ts
+++ frontend/Screens/FocusScreen/FocusScreen_Style.ts
@@ -76,54 +76,21 @@ export const styles = StyleSheet.create({
     marginVertical: 5,
 
   },
-  
+
+  modalHeader: {
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  pageInfo: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#666',
+  },
   
 });
-
-
-//Gammel kode
-
-// import { StyleSheet } from 'react-native';
-
-// export const styles = StyleSheet.create({
-//   container: { flex: 1, backgroundColor: "#f2f2f2" },
-//   content: { flexGrow: 1, paddingTop: 20 },
-//   questionContainer: {
-//     borderBottomWidth: 1,
-//     borderBottomColor: "#ececec",
-//     padding: 10,
-//   },
-//   questionText: { fontSize: 14, color: "#333" },
-//   optionButton: {
-//     padding: 10,
-//     borderRadius: 5,
-//     margin: 2,
-//     backgroundColor: "#dddddd",
-//   },
-//   selectedOption: { backgroundColor: "green" },
-//   optionText: { fontSize: 12, color: "black", textAlign: "center" },
-//   modalView: {
-//     marginHorizontal: 20,
-//     marginTop: "10%",
-//     marginBottom: "10%",
-//     paddingHorizontal: 15,
-//     paddingTop: 15,
-//     paddingBottom: 20,
-//     borderRadius: 20,
-//     backgroundColor: "white",
-//     shadowColor: "#000",
-//     shadowOffset: { width: 0, height: 2 },
-//     shadowOpacity: 0.25,
-//     shadowRadius: 3.84,
-//     elevation: 5,
-//     width: "100%",
-//     maxHeight: "85%",
-//   },
-//   diagnosisTitle: { fontWeight: "bold", fontSize: 18, marginTop: 20 },
-//   diagnosisText: { fontSize: 16, marginTop: 10, marginBottom: 20 },
-//   exerciseDetailContainer: { marginBottom: 20 },
-//   exerciseTitle: { fontSize: 18, fontWeight: "bold" },
-//   exerciseDescription: { fontSize: 14, marginVertical: 10 },
-//   exerciseImage: { width: 250, height: 150, resizeMode: "contain" },
-
-// });
