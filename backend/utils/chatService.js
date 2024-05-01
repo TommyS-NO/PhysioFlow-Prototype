98b13bb3e3cd91eae16d897fe4b6b22521cc9f8b
TommyS-NO <tommy.skorge@hotmail.com>
2024-05-01 21:42:41 +0200

    inprogress to add data from Vector and backend

--- backend/utils/chatService.js
+++ backend/utils/chatService.js
@@ -1,4 +1,5 @@
 import openai from "../openAI/openAIConfig.js";
+import { search } from "./vectorStoreService.js";
 
 const chatSessions = new Map();
 
@@ -29,13 +30,34 @@ export const sendMessage = async (sessionId, message) => {
 		content: message,
 	});
 
+	// Utfør søk for å finne lokale svar først
+	const localResults = search(message);
+	console.log("Search results:", localResults); // Logg resultater fra søket
+	if (localResults.length > 0 && localResults[0].content) {
+		console.log(`Local response found for session ${sessionId}`);
+		const aiMessageContent = `LocalInfo: ${localResults[0].content}`;
+		session.messages.push({
+			role: "assistant",
+			content: aiMessageContent,
+		});
+
+		console.log(
+			`Local response added to session ${sessionId}: ${aiMessageContent}`,
+		);
+		return {
+			messages: session.messages,
+			aiResponse: aiMessageContent,
+		};
+	}
+
+	// Hvis ikke noe lokalt svar er funnet, spør OpenAI
 	try {
 		console.log(`Fetching AI's response for session ${sessionId}`);
 		console.log("Session messages:", session.messages);
 		const chatCompletion = await openai.chat.completions.create({
 			model: "gpt-3.5-turbo",
 			messages: session.messages,
-			max_tokens: 150,
+			max_tokens: 650,
 			temperature: 0.7,
 			stop: ["\n"],
 			user: "BobAi",
