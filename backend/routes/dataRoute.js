8e4ade110c7bbf28a63207fc246b96c2c6243dd8
TommyS-NO <tommy.skorge@hotmail.com>
2024-05-01 21:44:57 +0200

    new dataroute

---
+++ backend/routes/dataRoute.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const dataController = require("../controllers/DataController");
+
+// Endepunkt for søk
+router.get("/search", dataController.searchData);
+
+module.exports = router;
