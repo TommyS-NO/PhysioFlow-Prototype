a2f3bf4d0204b7c462de389fa1a87e98405a1301
Minathuve <mina.t.andersen@gmail.com>
2024-05-03 22:23:12 +0200

    Commented out Tab code in component- Temporary

--- frontend/Components/TabNavigation/TabNavigation.tsx
+++ frontend/Components/TabNavigation/TabNavigation.tsx
@@ -1,34 +1,36 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import ProfileScreen from '../../Screens/ProfileScreen';
-import SettingsScreen from '../../Screens/SettingsScreen/SettingsScreen';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+//Foreløpig ikke i bruk. Har satt opp kode direkte i fil for å test først. 
 
-const Tab = createBottomTabNavigator();
+// import React from 'react';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import ProfileScreen from '../../Screens/ProfileScreen';
+// import SettingsScreen from '../../Screens/SettingsScreen/SettingsScreen';
+// import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const NavigationTabs = () => {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-          if (route.name === 'Profile') {
-            iconName = focused ? 'account-circle' : 'account-circle-outline';
-          } else if (route.name === 'Settings') {
-            iconName = focused ? 'settings' : 'settings-outline';
-          }
-          return <Icon name={iconName} size={size} color={color} />;
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: 'tomato',
-        inactiveTintColor: 'gray',
-      }}
-    >
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-    </Tab.Navigator>
-  );
-};
+// const Tab = createBottomTabNavigator();
 
-export default NavigationTabs;
+// const NavigationTabs = () => {
+//   return (
+//     <Tab.Navigator
+//       screenOptions={({ route }) => ({
+//         tabBarIcon: ({ focused, color, size }) => {
+//           let iconName;
+//           if (route.name === 'Profile') {
+//             iconName = focused ? 'account-circle' : 'account-circle-outline';
+//           } else if (route.name === 'Settings') {
+//             iconName = focused ? 'settings' : 'settings-outline';
+//           }
+//           return <Icon name={iconName} size={size} color={color} />;
+//         },
+//       })}
+//       tabBarOptions={{
+//         activeTintColor: 'tomato',
+//         inactiveTintColor: 'gray',
+//       }}
+//     >
+//       <Tab.Screen name="Profile" component={ProfileScreen} />
+//       <Tab.Screen name="Settings" component={SettingsScreen} />
+//     </Tab.Navigator>
+//   );
+// };
+
+// export default NavigationTabs;
