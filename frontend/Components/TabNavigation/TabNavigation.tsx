e9e4bf56f7eac05e992b9e3e81b709f9021a3220
Minathuve <mina.t.andersen@gmail.com>
2024-05-04 22:12:58 +0200

    Testcode to TabNav. Commented out temporary

--- frontend/Components/TabNavigation/TabNavigation.tsx
+++ frontend/Components/TabNavigation/TabNavigation.tsx
@@ -1,36 +1,40 @@
-//Foreløpig ikke i bruk. Har satt opp kode direkte i fil for å test først. 
+//Denne fungerer men det mangler koblinger som gjør at ikke navigasjonen fungerer.. 
 
 // import React from 'react';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 // import ProfileScreen from '../../Screens/ProfileScreen';
 // import SettingsScreen from '../../Screens/SettingsScreen/SettingsScreen';
-// import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 // const Tab = createBottomTabNavigator();
 
-// const NavigationTabs = () => {
-//   return (
-//     <Tab.Navigator
-//       screenOptions={({ route }) => ({
-//         tabBarIcon: ({ focused, color, size }) => {
-//           let iconName;
-//           if (route.name === 'Profile') {
-//             iconName = focused ? 'account-circle' : 'account-circle-outline';
-//           } else if (route.name === 'Settings') {
-//             iconName = focused ? 'settings' : 'settings-outline';
-//           }
-//           return <Icon name={iconName} size={size} color={color} />;
-//         },
-//       })}
-//       tabBarOptions={{
-//         activeTintColor: 'tomato',
-//         inactiveTintColor: 'gray',
-//       }}
-//     >
-//       <Tab.Screen name="Profile" component={ProfileScreen} />
-//       <Tab.Screen name="Settings" component={SettingsScreen} />
-//     </Tab.Navigator>
-//   );
-// };
+// const TabNavigation: React.FC = () => {
+//     return (
+//       <Tab.Navigator
+//         screenOptions={({ route }) => ({
+//           tabBarIcon: ({ focused, color, size }) => {
+//             let iconName;
+//             if (route.name === 'Hjem') {
+//               iconName = focused ? 'home' : 'home-outline';
+//             } else if (route.name === 'Innstillinger') {
+//               iconName = focused ? 'settings' : 'settings-outline';
+//             }
+//             return <Icon name={iconName} size={size} color={color} />;
+//           },
+//         })}
+//         tabBarOptions={{
+//           activeTintColor: '#26807C',
+//           inactiveTintColor: 'gray',
+//         }}
+//       >
+//         <Tab.Screen name="Hjem" component={ProfileScreen} />
+//         <Tab.Screen name="Innstillinger" component={SettingsScreen} />
+//       </Tab.Navigator>
+//     );
+//   }
+  
+//   export default TabNavigation;
 
-// export default NavigationTabs;
+
+
+//Koblet opp mot App.tsx
