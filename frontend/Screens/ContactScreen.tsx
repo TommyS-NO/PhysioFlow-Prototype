528a27c50953e358ef3fd1815a9f3015896922c6
Minathuve <mina.t.andersen@gmail.com>
2024-05-08 21:12:25 +0200

    Added footer on ContactScreen and deleted old code

--- frontend/Screens/ContactScreen.tsx
+++ frontend/Screens/ContactScreen.tsx
@@ -1,125 +1,3 @@
-// import React, { useState } from 'react';
-// import {
-//   View,
-//   Text,
-//   KeyboardAvoidingView,
-//   ScrollView,
-//   Platform,
-//   Alert,
-//   StyleSheet,
-// } from 'react-native';
-// import { openComposer } from 'react-native-email-link';
-// import CustomButton from "../Components/CustomButton/CustomButton";
-// import { InputField } from "../Components/CustomInput/CustomInput";
-// import { theme } from "../theme";
-
-// const ContactScreen = () => {
-//   const [name, setName] = useState('');
-//   const [email, setEmail] = useState('');
-//   const [phone, setPhone] = useState('');
-//   const [message, setMessage] = useState('');
-
-//   const handleSendEmail = () => {
-//     if (!name.trim() || !email.trim() || !message.trim()) {
-//       Alert.alert("Feil", "Alle felt må fylles ut.");
-//       return;
-//     }
-//     openComposer({
-//       to: 'bobAI_saeed@hotmail.com',
-//       subject: 'Henvendelse fra app',
-//       body: `Navn: ${name}\nEpost: ${email}\nTelefon: ${phone}\nMelding: ${message}`
-//     });
-//   };
-
-  // return (
-  //   <KeyboardAvoidingView
-  //     style={{ flex: 1 }}
-  //     behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-  //     keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}>
-  //     <ScrollView style={styles.container}>
-  //     <View style={styles.formContainer}> 
-  //       <Text style={styles.headerText}>Kontakt oss ☎️</Text>
-  //       <InputField
-  //         label="Navn"
-  //         value={name}
-  //         onChangeText={setName}
-  //         style={styles.input}
-  //       />
-  //       <InputField
-  //         label="E-post"
-  //         value={email}
-  //         onChangeText={setEmail}
-  //         keyboardType="email-address"
-  //         style={styles.input}
-  //       />
-  //       <InputField
-  //         label="Mobilnummer"
-  //         value={phone}
-  //         onChangeText={setPhone}
-  //         keyboardType="phone-pad"
-  //         style={styles.input}
-  //       />
-//         <InputField
-//           label="Hva kan vi hjelpe deg med?"
-//           value={message}
-//           onChangeText={setMessage}
-//           multiline
-//           numberOfLines={10}
-//           style={styles.textArea}
-//         />
-//         <CustomButton
-//           title="Send melding "
-//           onPress={handleSendEmail}
-//           iconName="send"
-//         />
-//         </View>
-//       </ScrollView>
-//     </KeyboardAvoidingView>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     padding: 20,
-//     backgroundColor: theme.colors.primary,
-//   },
-//   headerText: {
-//     fontSize: 22,
-//     fontWeight: "bold",
-//     marginBottom: 20,
-//     textAlign: "center",
-//   },
-//   input: {
-//     backgroundColor: "#fff",
-//     padding: 10,
-//     borderRadius: 8,
-//     borderWidth: 1,
-//     borderColor: "#ddd",
-//     marginBottom: 1,
-//   },
-//   textArea: {
-//     backgroundColor: "#fff",
-//     padding: 10,
-//     borderRadius: 10,
-//     borderWidth: 1,
-//     borderColor: "#ddd",
-//     textAlignVertical: "top",
-//     marginBottom: 20,
-//     height: 150,
-//   },
-//   formContainer: {
-//     backgroundColor: "#FFFFFF",
-//     padding: 15,
-//     marginTop: 20,
-//     borderRadius: 8,
-//     marginBottom: 15,
-//     alignItems: 'center',
-//   },
-// });
-
-// export default ContactScreen;
-
 import React, { useState } from 'react';
 import {
   View,
@@ -132,6 +10,7 @@ import CustomButton from "../Components/CustomButton/CustomButton";
 import { InputField } from "../Components/CustomInput/CustomInput"; 
 import { theme } from "../theme";
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import FooterNavigation from "../Components/FooterNavigation/FooterNavigation";
 
 const ContactScreen = () => {
   const [name, setName] = useState('');
@@ -164,8 +43,10 @@ const ContactScreen = () => {
   };
 
   return (
+    <View style={styles.container}>
+      <View></View>
     <KeyboardAwareScrollView
-      style={{ flex: 1 }}
+      style={{ }}
       resetScrollToCoords={{ x: 0, y: 0 }}
       contentContainerStyle={styles.container}
       scrollEnabled={true}
@@ -207,20 +88,25 @@ const ContactScreen = () => {
           iconName="send"
         />
         </View>
+  
+        
         </KeyboardAwareScrollView>
+        <FooterNavigation />
+        </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    backgroundColor: theme.colors.primary,
+   
+
   },
   headerText: {
     fontSize: 22,
     fontWeight: "bold",
     marginBottom: 20,
+    marginTop: 20,
     textAlign: "center",
   },
   input: {
