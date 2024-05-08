b8329352bd49eb9ec5be879f49608a4890317746
TommyS-NO <tommy.skorge@hotmail.com>
2024-05-08 22:34:45 +0200

    updated and done

--- backend/controllers/surveyController.js
+++ backend/controllers/surveyController.js
@@ -1,38 +1,10 @@
-import { ankleSurvey } from "../surveys/ankleSurvey.js";
-import { ankleDiagnoses } from "../diagnoses/ankleDiagnoses.js";
-import { elbowSurvey } from "../surveys/elbowSurvey.js";
-import { elbowDiagnoses } from "../diagnoses/elbowDiagnoses.js";
-import { hipSurvey } from "../surveys/hipSurvey.js";
-import { hipDiagnoses } from "../diagnoses/hipDiagnoses.js";
-import { kneeSurvey } from "../surveys/kneeSurvey.js";
-import { kneeDiagnoses } from "../diagnoses/kneeDiagnoses.js";
-import { lowBackSurvey } from "../surveys/lowBackSurvey.js";
-import { lowBackDiagnoses } from "../diagnoses/lowBackDiagnoses.js";
-import { neckSurvey } from "../surveys/neckSurvey.js";
-import { neckDiagnoses } from "../diagnoses/neckDiagnoses.js";
-import { shoulderSurvey } from "../surveys/shoulderSurvey.js";
-import { shoulderDiagnoses } from "../diagnoses/shoulderDiagnoses.js";
-import { upperBackSurvey } from "../surveys/upperBackSurvey.js";
-import { upperBackDiagnoses } from "../diagnoses/upperBackDiagnoses.js";
-import { wristSurvey } from "../surveys/wristSurvey.js";
-import { wristDiagnoses } from "../diagnoses/wristDiagnoses.js";
-import { followupSurvey } from "../surveys/followUpSurvey.js";
+import surveys from "../surveys/index.js";
+import diagnoses from "../diagnoses/index.js";
 import { interpretAnswers } from "../utils/surveyUtils.js";
 import { NotFoundError, ValidationError } from "../utils/customError.js";
 
 const surveyController = {
-	surveys: {
-		ankle: ankleSurvey,
-		elbow: elbowSurvey,
-		hip: hipSurvey,
-		knee: kneeSurvey,
-		lowBack: lowBackSurvey,
-		neck: neckSurvey,
-		shoulder: shoulderSurvey,
-		upperBack: upperBackSurvey,
-		wrist: wristSurvey,
-		followup: followupSurvey,
-	},
+	surveys: surveys,
 
 	evaluateDiagnosis: function (answers, surveyId) {
 		const survey = this.surveys[surveyId];
@@ -40,32 +12,21 @@ const surveyController = {
 			throw new NotFoundError(`Undersøkelse ${surveyId} ikke funnet`);
 		}
 
-		const diagnosesMapping = {
-			ankle: ankleDiagnoses,
-			elbow: elbowDiagnoses,
-			hip: hipDiagnoses,
-			knee: kneeDiagnoses,
-			lowBack: lowBackDiagnoses,
-			neck: neckDiagnoses,
-			shoulder: shoulderDiagnoses,
-			upperBack: upperBackDiagnoses,
-			wrist: wristDiagnoses,
-		};
-
-		const diagnoses = diagnosesMapping[surveyId];
-		if (!diagnoses) {
+		const surveyDiagnoses = diagnoses[surveyId];
+		if (!surveyDiagnoses) {
 			throw new NotFoundError(`Diagnoser for ${surveyId} ikke funnet`);
 		}
 
 		const symptoms = interpretAnswers(answers, survey.questions);
-
 		const diagnosisScores = {};
-		for (const diagnosis in diagnoses) {
+		for (const diagnosis in surveyDiagnoses) {
 			diagnosisScores[diagnosis] = 0;
 		}
 
 		for (const symptom of symptoms) {
-			for (const [diagnosis, { keySymptoms }] of Object.entries(diagnoses)) {
+			for (const [diagnosis, { keySymptoms }] of Object.entries(
+				surveyDiagnoses,
+			)) {
 				if (keySymptoms.includes(symptom)) {
 					diagnosisScores[diagnosis]++;
 				}
@@ -84,9 +45,9 @@ const surveyController = {
 
 		if (highestScore >= 3) {
 			return {
-				diagnosis: diagnoses[finalDiagnosis].name,
-				description: diagnoses[finalDiagnosis].description,
-				exercises: diagnoses[finalDiagnosis].recommendedExercises,
+				diagnosis: surveyDiagnoses[finalDiagnosis].name,
+				description: surveyDiagnoses[finalDiagnosis].description,
+				exercises: surveyDiagnoses[finalDiagnosis].recommendedExercises,
 			};
 		}
 
