80f5e447661c1dda879f807a5d2d40b7549bfb84
Minathuve <mina.t.andersen@gmail.com>
2024-05-09 09:44:53 +0200

    Added new btns on CustomBtn- Comment out old btn

--- frontend/Components/CustomButton/CustomButton.tsx
+++ frontend/Components/CustomButton/CustomButton.tsx
@@ -1,10 +1,94 @@
+// import React from "react";
+// import {
+// 	StyleSheet,
+// 	Text,
+// 	TouchableOpacity,
+// 	ViewStyle,
+// 	TextStyle,
+// } from "react-native";
+// import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+// import { theme } from "../../theme";
+
+// interface CustomButtonProps {
+// 	title?: string;
+// 	onPress: () => void;
+// 	iconName?: string;
+// 	buttonStyle?: ViewStyle;
+// 	titleStyle?: TextStyle;
+// 	disabled?: boolean;
+// }
+
+// const CustomButton = ({
+// 	title,
+// 	onPress,
+// 	iconName,
+// 	buttonStyle,
+// 	titleStyle,
+// 	disabled = false,
+// }: CustomButtonProps) => {
+// 	return (
+// 		<TouchableOpacity
+// 			onPress={onPress}
+// 			style={[styles.button, buttonStyle, disabled ? styles.disabled : {}]}
+// 			disabled={disabled}
+// 		>
+// 			{iconName && (
+// 				<Icon
+// 					name={iconName}
+// 					size={20}
+// 					color={disabled ? theme.colors.disabled : theme.colors.icon}
+// 					style={styles.icon}
+// 				/>
+// 			)}
+// 			<Text
+// 				style={[styles.title, titleStyle, disabled ? styles.disabledTitle : {}]}
+// 			>
+// 				{title}
+// 			</Text>
+// 		</TouchableOpacity>
+// 	);
+// };
+
+// const styles = StyleSheet.create({
+// 	button: {
+// 		flexDirection: "row",
+// 		alignItems: "center",
+// 		justifyContent: "center",
+// 		paddingVertical: theme.spacing.medium,
+// 		paddingHorizontal: theme.spacing.medium,
+// 		backgroundColor: theme.colors.button,
+// 		width: "100%",
+// 		alignSelf: "center",
+// 		margin: theme.spacing.small,
+// 		marginHorizontal: 4,
+// 		borderRadius: theme.borderRadius.medium,
+// 	},
+// 	title: {
+// 		fontSize: theme.fontSize.regular,
+// 		color: theme.colors.buttonText,
+// 	},
+// 	icon: {
+// 		marginRight: theme.spacing.small,
+// 	},
+// 	disabled: {
+// 		backgroundColor: theme.colors.disabled,
+// 	},
+// 	disabledTitle: {
+// 		color: theme.colors.disabledText,
+// 	},
+// });
+
+// export default CustomButton;
+
+
+//Ny kode med flere størrelsesalternativer. Gammel kode er kommentert ut. 
 import React from "react";
 import {
-	StyleSheet,
-	Text,
-	TouchableOpacity,
-	ViewStyle,
-	TextStyle,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  ViewStyle,
+  TextStyle,
 } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { theme } from "../../theme";
@@ -16,66 +100,99 @@ interface CustomButtonProps {
 	buttonStyle?: ViewStyle;
 	titleStyle?: TextStyle;
 	disabled?: boolean;
-}
-
-const CustomButton = ({
+	size?: "small" | "medium" | "large";
+  }
+  
+  const CustomButton = ({
 	title,
 	onPress,
 	iconName,
 	buttonStyle,
 	titleStyle,
 	disabled = false,
-}: CustomButtonProps) => {
+	size = "large",
+  }: CustomButtonProps) => {
+	const buttonSizeStyle = styles[size]; 
+	const buttonColorStyle = disabled ? styles.disabled : styles[`${size}Color`];
+  
 	return (
-		<TouchableOpacity
-			onPress={onPress}
-			style={[styles.button, buttonStyle, disabled ? styles.disabled : {}]}
-			disabled={disabled}
+	  <TouchableOpacity
+		onPress={onPress}
+		style={[
+		  styles.button,
+		  buttonSizeStyle,
+		  buttonColorStyle,
+		  buttonStyle,
+		  disabled ? styles.disabled : {},
+		]}
+		disabled={disabled}
+	  >
+		{iconName && (
+		  <Icon
+			name={iconName}
+			size={20}
+			color={disabled ? theme.colors.disabled : theme.colors.icon}
+			style={styles.icon}
+		  />
+		)}
+		<Text
+		  style={[
+			styles.title,
+			titleStyle,
+			disabled ? styles.disabledTitle : {},
+		  ]}
 		>
-			{iconName && (
-				<Icon
-					name={iconName}
-					size={20}
-					color={disabled ? theme.colors.disabled : theme.colors.icon}
-					style={styles.icon}
-				/>
-			)}
-			<Text
-				style={[styles.title, titleStyle, disabled ? styles.disabledTitle : {}]}
-			>
-				{title}
-			</Text>
-		</TouchableOpacity>
+		  {title}
+		</Text>
+	  </TouchableOpacity>
 	);
-};
-
-const styles = StyleSheet.create({
+  };
+  
+  const styles = StyleSheet.create({
 	button: {
-		flexDirection: "row",
-		alignItems: "center",
-		justifyContent: "center",
-		paddingVertical: theme.spacing.medium,
-		paddingHorizontal: theme.spacing.medium,
-		backgroundColor: theme.colors.button,
-		width: "100%",
-		alignSelf: "center",
-		margin: theme.spacing.small,
-		marginHorizontal: 4,
-		borderRadius: theme.borderRadius.medium,
+	  flexDirection: "row",
+	  alignItems: "center",
+	  justifyContent: "center",
+	  width: "100%",
+	  alignSelf: "center",
+	  borderRadius: theme.borderRadius.medium,
+	  margin: theme.spacing.small,
+	  paddingHorizontal: theme.spacing.medium,
+	  
+	},
+	large: {
+	  paddingVertical: theme.spacing.medium,
+	},
+	medium: {
+	  paddingVertical: theme.spacing.medium - 4, 
+	  width: "60%",
+	},
+	small: {
+	  paddingVertical: theme.spacing.small,
+	  width: "75%",
+	},
+	largeColor: {
+	  backgroundColor: theme.colors.button,
+	},
+	mediumColor: {
+	  backgroundColor: theme.colors.primaryButton, 
+	},
+	smallColor: {
+	  backgroundColor: theme.colors.primaryButton,
 	},
 	title: {
-		fontSize: theme.fontSize.regular,
-		color: theme.colors.buttonText,
+	  fontSize: theme.fontSize.regular,
+	  color: theme.colors.buttonText,
 	},
 	icon: {
-		marginRight: theme.spacing.small,
+	  marginRight: theme.spacing.small,
 	},
 	disabled: {
-		backgroundColor: theme.colors.disabled,
+	  backgroundColor: theme.colors.disabled,
 	},
 	disabledTitle: {
-		color: theme.colors.disabledText,
+	  color: theme.colors.disabledText,
 	},
-});
-
-export default CustomButton;
+  });
+  
+  export default CustomButton;
