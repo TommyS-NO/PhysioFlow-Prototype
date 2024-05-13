d15704fad257523ea0a60fd264c76e518e570275
TommyS-NO <tommy.skorge@hotmail.com>
2024-05-13 14:00:53 +0200

    clean up

--- frontend/Validation/useFormValidation.ts
+++ frontend/Validation/useFormValidation.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 interface ValidationErrors {
   [key: string]: string | undefined;
@@ -13,26 +13,22 @@ function useFormValidation<T>(initialState: T, validate: ValidateFunction<T>) {
   const [errors, setErrors] = useState<ValidationErrors>({});
 
   useEffect(() => {
-    if (validate) {
-      const validationErrors = validate(values);
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+  }, [values, validate]);
+
+  const handleChange = useCallback(<K extends keyof T>(field: K, value: T[K]) => {
+    setValues(prevValues => ({ ...prevValues, [field]: value }));
+  }, []);
+
+  const handleSubmit = useCallback((callback: () => void) => {
+    const validationErrors = validate(values);
+    if (Object.keys(validationErrors).length === 0) {
+      callback();
+    } else {
       setErrors(validationErrors);
     }
   }, [values, validate]);
-
-  const handleChange = <K extends keyof T>(field: K, value: T[K]) => {
-    setValues((prevValues) => ({ ...prevValues, [field]: value }));
-  };
-
-  const handleSubmit = (callback: () => void) => {
-    if (validate) {
-      const validationErrors = validate(values);
-      if (Object.keys(validationErrors).length === 0) {
-        callback();
-      } else {
-        setErrors(validationErrors);
-      }
-    }
-  };
 
   return {
     values,
