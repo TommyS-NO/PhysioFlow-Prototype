d1512e22c2a654010d71990ef8b247752f69acbd
TommyS-NO <tommy.skorge@hotmail.com>
2024-05-14 22:33:12 +0200

    customcheck

---
+++ SLETT/CustomCheckBox/CustomCheckBox.tsx
@@ -0,0 +1,23 @@
+// import React from "react";
+// import { CheckBox } from "react-native-elements";
+// import { ViewStyle } from "react-native";
+
+// interface CustomCheckBoxProps {
+// 	title: string;
+// 	checked: boolean;
+// 	onPress: (title: string) => void;
+// 	containerStyle?: ViewStyle;
+// }
+
+// const CustomCheckBox: React.FC<CustomCheckBoxProps> = ({
+// 	title,
+// 	checked,
+// 	onPress,
+// 	containerStyle,
+// }) => {
+// 	return (
+// 		<CheckBox title={title} checked={checked} onPress={() => onPress(title)} />
+// 	);
+// };
+
+// export default CustomCheckBox;
