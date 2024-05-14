374d3509a50d99cedc45c4f0c7eba11afc2fbb5e
TommyS-NO <tommy.skorge@hotmail.com>
2024-05-14 22:32:28 +0200

    Your commit message

--- frontend/Components/CustomButton/CustomButton.tsx
+++ frontend/Components/CustomButton/CustomButton.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import {
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  ViewStyle,
-  TextStyle,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	ViewStyle,
+	TextStyle,
 } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { theme } from "../../theme";
 
 interface CustomButtonProps {
-	title?: string;
+	title: string;
 	onPress: () => void;
 	iconName?: string;
 	buttonStyle?: ViewStyle;
 	titleStyle?: TextStyle;
 	disabled?: boolean;
 	size?: "small" | "medium" | "large";
-  }
-  
-  const CustomButton = ({
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({
 	title,
 	onPress,
 	iconName,
@@ -27,88 +27,83 @@ interface CustomButtonProps {
 	titleStyle,
 	disabled = false,
 	size = "large",
-  }: CustomButtonProps) => {
-	const buttonSizeStyle = styles[size]; 
+}) => {
+	const buttonSizeStyle = styles[size];
 	const buttonColorStyle = disabled ? styles.disabled : styles[`${size}Color`];
-  
+
 	return (
-	  <TouchableOpacity
-		onPress={onPress}
-		style={[
-		  styles.button,
-		  buttonSizeStyle,
-		  buttonColorStyle,
-		  buttonStyle,
-		  disabled ? styles.disabled : {},
-		]}
-		disabled={disabled}
-	  >
-		{iconName && (
-		  <Icon
-			name={iconName}
-			size={20}
-			color={disabled ? theme.colors.disabled : theme.colors.icon}
-			style={styles.icon}
-		  />
-		)}
-		<Text
-		  style={[
-			styles.title,
-			titleStyle,
-			disabled ? styles.disabledTitle : {},
-		  ]}
+		<TouchableOpacity
+			onPress={onPress}
+			style={[
+				styles.button,
+				buttonSizeStyle,
+				buttonColorStyle,
+				buttonStyle,
+				disabled ? styles.disabled : {},
+			]}
+			disabled={disabled}
 		>
-		  {title}
-		</Text>
-	  </TouchableOpacity>
+			{iconName && (
+				<Icon
+					name={iconName}
+					size={20}
+					color={disabled ? theme.colors.disabled : theme.colors.icon}
+					style={styles.icon}
+				/>
+			)}
+			<Text
+				style={[styles.title, titleStyle, disabled ? styles.disabledTitle : {}]}
+			>
+				{title}
+			</Text>
+		</TouchableOpacity>
 	);
-  };
-  
-  const styles = StyleSheet.create({
+};
+
+const styles = StyleSheet.create({
 	button: {
-	  flexDirection: "row",
-	  alignItems: "center",
-	  justifyContent: "center",
-	  width: "100%",
-	  alignSelf: "center",
-	  borderRadius: theme.borderRadius.medium,
-	  margin: theme.spacing.small,
-	  paddingHorizontal: theme.spacing.medium,
-	  
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "center",
+		width: "100%",
+		alignSelf: "center",
+		borderRadius: theme.borderRadius.medium,
+		margin: theme.spacing.small,
+		paddingHorizontal: theme.spacing.medium,
 	},
 	large: {
-	  paddingVertical: theme.spacing.medium,
+		paddingVertical: theme.spacing.medium,
 	},
 	medium: {
-	  paddingVertical: theme.spacing.medium - 4, 
-	  width: "60%",
+		paddingVertical: theme.spacing.medium - 4,
+		width: "60%",
 	},
 	small: {
-	  paddingVertical: theme.spacing.small,
-	  width: "75%",
+		paddingVertical: theme.spacing.small,
+		width: "75%",
 	},
 	largeColor: {
-	  backgroundColor: theme.colors.button,
+		backgroundColor: theme.colors.button,
 	},
 	mediumColor: {
-	  backgroundColor: theme.colors.primaryButton, 
+		backgroundColor: theme.colors.primaryButton,
 	},
 	smallColor: {
-	  backgroundColor: theme.colors.primaryButton,
+		backgroundColor: theme.colors.primaryButton,
 	},
 	title: {
-	  fontSize: theme.fontSize.regular,
-	  color: theme.colors.buttonText,
+		fontSize: theme.fontSize.regular,
+		color: theme.colors.buttonText,
 	},
 	icon: {
-	  marginRight: theme.spacing.small,
+		marginRight: theme.spacing.small,
 	},
 	disabled: {
-	  backgroundColor: theme.colors.disabled,
+		backgroundColor: theme.colors.disabled,
 	},
 	disabledTitle: {
-	  color: theme.colors.disabledText,
+		color: theme.colors.disabledText,
 	},
-  });
-  
-  export default CustomButton;
+});
+
+export default CustomButton;
